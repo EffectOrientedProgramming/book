d9833b6b942b9788a05d1c6f53aaf17be64c2e1e
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2021-05-31 11:29:00 -0600

    Add Finalizer Example #wyett #time 1h

--- src/main/scala/Parallelism/Finalizers.scala
+++ src/main/scala/Parallelism/Finalizers.scala
@@ -1,3 +1,37 @@
 package Parallelism
 
-class Finalizers {}
+import antipatterns.SomeNewClass
+
+import java.io.IOException
+import zio.console.{Console, getStrLn, putStrLn}
+import zio.{Fiber, IO, Runtime, UIO, ZIO, ZLayer}
+
+import scala.io.Source._
+
+object Finalizers extends zio.App{
+
+  val importedFile: ZIO[Console, IOException, Vector[String]] =
+    val bufferedSource = scala.io.Source.fromFile("src/main/scala/Parallelism/csvFile.csv")
+
+    val lines = for
+      line <- bufferedSource.getLines
+    yield line
+    ZIO.succeed(Vector() ++ lines)
+
+
+  def run(args: List[String]) = //Use App's run function
+    println("In main")
+
+    val ioExample:ZIO[Console, IOException, Unit] =
+      for
+        i <- importedFile
+        vector = i.flatMap(j => toString)
+        _ <- putStrLn(vector.toString)
+      yield ()
+    ioExample.exitCode //Call the Zio with exitCode.
+
+
+
+
+
+}
