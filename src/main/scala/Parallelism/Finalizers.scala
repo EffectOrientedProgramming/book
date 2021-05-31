b845c6a2db8d63520a20addce48d56a6b6b6a94e
bfrasure <bill.frasure@gmail.com>
2021-05-31 12:16:53 -0600

    Use bracket to ensure our close finalizer always happens #bill #time 15m

--- src/main/scala/Parallelism/Finalizers.scala
+++ src/main/scala/Parallelism/Finalizers.scala
@@ -4,48 +4,41 @@ import antipatterns.SomeNewClass
 
 import java.io.IOException
 import zio.console.{Console, getStrLn, putStrLn}
-import zio.{Fiber, IO, Runtime, UIO, ZIO, ZLayer}
+import zio.{Fiber, IO, Runtime, UIO, ZIO, URIO, ZLayer}
 
 import scala.io.Source._
 
-object Finalizers extends zio.App{
+object Finalizers extends zio.App {
 
-  val readFileContents: ZIO[Console, IOException, Vector[String]] =
-
-    ZIO.succeed {
-      val bufferedSource = scala.io.Source.fromFile("src/main/scala/Parallelism/csvFile.csv")
-
-      val lines = for
-        line <- bufferedSource.getLines
-      yield line
-
-      if (true)
-        throw new IOException("Boom!")
-
-      Vector() ++ lines
-    }.ensuring(finalizer)
-
-  val finalizer =  //Define the finalizer behavior here
-    UIO.effectTotal{
+  def finalizer(source: scala.io.Source) = //Define the finalizer behavior here
+    UIO.effectTotal {
       println("Finalizing: Closing file reader")
-      //bufferedSource.close
+      source.close
     }
 
+  val readFileContents: ZIO[Any, Throwable, Vector[String]] =
+    ZIO(scala.io.Source.fromFile("src/main/scala/Parallelism/csvFile.csv"))
+      .bracket(finalizer) { bufferedSource =>
 
+        val lines =
+          for line <- bufferedSource.getLines
+          yield line
+
+        if (true)
+          throw new IOException("Boom!")
+
+        ZIO.succeed(Vector() ++ lines)
+      }
 
   def run(args: List[String]) = //Use App's run function
     println("In main")
 
-    val ioExample:ZIO[Console, IOException, Unit] =
+    val ioExample: ZIO[Console, Throwable, Unit] =
       for
-      //First way of using a finalizer: When executing/interpreting a ZIO, use the .ensuring method with the finalizer value name.
+        //First way of using a finalizer: When executing/interpreting a ZIO, use the .ensuring method with the finalizer value name.
         fileLines <- readFileContents
         _ <- putStrLn(fileLines.mkString("\n"))
       yield ()
     ioExample.exitCode //Call the Zio with exitCode.
-
-
-
-
 
 }
