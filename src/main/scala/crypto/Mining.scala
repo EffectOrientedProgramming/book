b065beb93f71d9a6b3726f4b359381b2de1cf8eb
bfrasure <bill.frasure@gmail.com>
2021-05-31 20:18:58 -0600

    Getting closer to a legitimate crypto mining example #bill #time 10m

--- src/main/scala/crypto/Mining.scala
+++ src/main/scala/crypto/Mining.scala
@@ -8,18 +8,30 @@ import zio.random._
 
 object Mining extends zio.App {
 
-  def miner(name: String) =
-    for
-      duration <- nextInt.map(_.abs % 10 + 1)
-      _ <- ZIO.sleep(duration.second)
-    yield s"$name finished in $duration seconds"
+  class Miner(val name: String):
+    val mine =
+      for
+        duration <- nextInt.map(_.abs % 7 + 1)
+        _ <- ZIO.sleep(duration.second)
+      yield s"$name mined the next coin in $duration seconds"
+
+  // Nonempty list TODO embed in the type
+  def findNextBlock(miners: Seq[Miner]) =
+    miners
+      .map(_.mine)
+      .reduce { case (minersSoFar, nextMiner) => minersSoFar.race(nextMiner) }
 
   def run(args: List[String]) = //Use App's run function
+    val zeb = Miner("Zeb")
+    val frop = Miner("Frop")
+    val shtep = Miner("Shtep")
     val logic =
       for
-        raceResult <- miner("Zeb").race(miner("Frop"))
+        raceResult <- findNextBlock(Seq(zeb, frop, shtep))
         _ <- putStrLn("Winner: " + raceResult)
       yield ()
-    logic.exitCode
+    logic
+      .repeatN(5)
+      .exitCode
 
 }
