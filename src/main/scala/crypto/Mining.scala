c7ec67a74f8d1a1e908a71cd34bf6f4ee012e78a
bfrasure <bill.frasure@gmail.com>
2021-06-01 21:01:58 -0600

    Show alternate approaches to Mining example #bruce #wyett #james #bill #time 1h

--- src/main/scala/crypto/Mining.scala
+++ src/main/scala/crypto/Mining.scala
@@ -9,41 +9,42 @@ import zio.random._
 object Mining extends zio.App {
 
   case class Miner(val name: String):
-    val mine=
+    val mine =
       for
-        duration <- nextInt.map(_.abs % 7 + 1)
+        duration <- nextIntBetween(1, 7)
         _ <- ZIO.sleep(duration.second)
       yield (this, s"$name mined the next coin in $duration seconds")
 
     //Inefficiently determines if the input number is prime.
-    def isPrime(num: Long):Boolean =
-      for (i <- 2 to num.toInt-1)
-        if(num % i == 0)
+    def isPrime(num: Long): Boolean =
+      for (i <- 2 to num.toInt - 1)
+        if (num % i == 0)
           return false
       return true
 
     //Recursivley itterates up from starting value, num, until it finds a prime number, which it returns
-    def findNextPrime(num: Long):Long =
+    def findNextPrime(num: Long): Long =
       if (isPrime(num))
         num
       else
         findNextPrime(num + 1)
 
     //Takes a starting value, then calls itterates up through numbers until it finds a prime number.
-    def mine2(num:Long):ZIO[zio.random.Random & zio.Has[zio.clock.Clock.Service], Nothing, Object]=
+    def mine2(num: Long): ZIO[zio.random.Random & zio.Has[
+      zio.clock.Clock.Service
+    ], Nothing, Object] =
       for
         duration <- nextInt.map(_.abs % 3 + 1)
         _ <- ZIO.sleep(duration.second)
         prime = findNextPrime(num)
       yield s"$name mined the next coin at prime number: $prime"
 
-
   // Nonempty list TODO embed in the type
   def findNextBlock(miners: Seq[Miner]) =
     miners
       .map(_.mine)
 //      .reduce { case (minersSoFar, nextMiner) => minersSoFar.race(nextMiner) }
-      .reduce(_.race(_)) // Much terser. I think it's worth using this form
+      .reduce(_ race _) // Much terser. I think it's worth using this form
 
   def findNextBlock2(miners: Seq[Miner], startNum: Long) =
     miners
@@ -55,43 +56,77 @@ object Mining extends zio.App {
     val frop = Miner("Frop")
     val shtep = Miner("Shtep")
 
-    val logic1 =     //Uses mine1 function (Just sleeping)
+    val logic1 = //Uses mine1 function (Just sleeping)
       for
         raceResult <- findNextBlock(Seq(zeb, frop, shtep))
         (winner, winnerText) = raceResult
         _ <- putStrLn("Winner: " + winnerText)
       yield (winner)
 
-//    def recordWinner(coinAccounts: Ref[Map[Miner, Int]]) =
-//      for
-//        winner <- logic1
-//        currentCoins <- coinAccounts.get
-//        _ <- coinAccounts.set(currentCoins.put(winner, currentCoins(winner) + 1))
-//      yield ()
-
-    val logic2 =     //Uses mine2 function (sleep and find prime numbers)
+    def recordWinner(coinAccounts: Ref[Map[Miner, Int]]) =
       for
-        startNum <- nextInt.map(_.abs % 1000000 + 1000000)  //This is the value that the prime number finder starts from
-        raceResult <- findNextBlock2(Seq(zeb, frop, shtep), startNum )
-        _ <- putStrLn("Winner: " + raceResult )
+        winner <- logic1
+        currentCoins: Map[Miner, Int] <- coinAccounts.get
+        _ <- coinAccounts.set(
+          currentCoins.updated(winner, (currentCoins(winner) + 1))
+        )
       yield ()
 
-//      val miningWithResults =
-//          for
-//            coinResults <- Ref.make(Map(frop -> 0, zeb -> 0, shtep -> 0))
-//            _ <- recordWinner .repeatN(5)
-//          yield ()
+    def recordWinnerFold(coinAccounts: Map[Miner, Int]) =
+      for winner <- logic1
+      yield coinAccounts.updated(winner, (coinAccounts(winner) + 1))
 
+    val logic2 = //Uses mine2 function (sleep and find prime numbers)
+      for
+        startNum <- nextInt.map(
+          _.abs % 1000000 + 1000000
+        ) //This is the value that the prime number finder starts from
+        raceResult <- findNextBlock2(Seq(zeb, frop, shtep), startNum)
+        _ <- putStrLn("Winner: " + raceResult)
+      yield ()
 
-//    miningWithResults.exitCode
-  logic1
-    .repeatN(5)
-    .exitCode
+    def miningAttempt(
+        innards: ZIO[zio.random.Random & zio.Has[
+          zio.clock.Clock.Service
+        ] & zio.console.Console, java.io.IOException, Map[Miner, Int]]
+    ) =
+      for
+        finalCoinResults: Map[Miner, Int] <- innards
+        _ <- putStrLn(
+          finalCoinResults
+            .map { x =>
+              x.toString
+            }
+            .mkString("\n")
+        )
+      yield ()
 
-//    logic2
+    // Demonstrating Refs vs Fold. These 2 approaches are the exact same
+    val miningWithResults =
+      miningAttempt(
+        for
+          coinResults <- Ref.make(Map(frop -> 0, zeb -> 0, shtep -> 0))
+          _ <- recordWinner(coinResults).repeatN(5)
+          finalCoinResults: Map[Miner, Int] <- coinResults.get
+        yield finalCoinResults
+      )
+
+    val miningWithResultsFold =
+      miningAttempt(
+        ZIO.foldLeft((1 to 5))(Map(frop -> 0, zeb -> 0, shtep -> 0)) {
+          case (coinResults, _) => recordWinnerFold(coinResults)
+        }
+      )
+
+//    logic1
 //      .repeatN(5)
 //      .exitCode
 
+//    miningWithResults.exitCode
 
+    miningWithResultsFold.exitCode
+//    logic2
+//      .repeatN(5)
+//      .exitCode
 
 }
