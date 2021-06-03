914c087bae68f92ac92b908cf0743e51444146e4
bfrasure <bill.frasure@gmail.com>
2021-06-02 20:15:50 -0600

    Make mining parallel. To _actually_ show a meaningful race #bill #time 40m Now the competitors are winning in true proportion to their resources

--- src/main/scala/crypto/Mining.scala
+++ src/main/scala/crypto/Mining.scala
@@ -9,14 +9,14 @@ import zio.random._
 object Mining extends zio.App {
 
   case class Miner(val name: String, gigaFlops: Int):
-    val mineBySleeping =
+    def mineBySleeping(maxSleepSeconds: Int) =
       for
-        duration <- nextIntBetween(1, 7)
+        duration <- nextIntBetween(1, maxSleepSeconds)
         _ <- ZIO.sleep(duration.second)
       yield (this, s"$name mined the next coin in $duration seconds")
 
     //Takes a starting value, then calls itterates up through numbers until it finds a prime number.
-    def mineByCalculatingPrimeNumbers(num: Long) =
+    def mineByCalculatingPrimeNumbers(num: Long): ZIO[Any, Nothing, (Miner, String)] =
       ZIO.succeed(
         (
           this,
@@ -24,29 +24,53 @@ object Mining extends zio.App {
         )
       )
 
+
   // Nonempty list TODO embed in the type
-  def findNextBlock(miners: Seq[Miner]) =
+  def competeForNextBlock(miners: Seq[Miner], f: Miner => ZIO[zio.random.Random & zio.Has[zio.clock.Clock.Service] & zio.Has[zio.console.Console.Service],
+    Exception, (Miner, String)]) =
     miners
-      .map(_.mineBySleeping)
+      .map(f(_))
+      //      .foldLeft(()) // TODO Consider a cheating opportunity
       .reduce(_ race _) // Much terser. I think it's worth using this form
 
-  def findNextBlock2(miners: Seq[Miner], startNum: Long) =
-    miners
-      .map(_.mineByCalculatingPrimeNumbers(startNum))
-      .reduce(_.race(_))
+  // Nonempty list TODO embed in the type
+  def competeForNextBlockPar(miners: Seq[Miner], f: Miner => ZIO[zio.random.Random & zio.Has[zio.clock.Clock.Service] &
+    zio.Has[zio.console.Console.Service],
+    Exception, (Miner, String)]) =
+    val (head :: tail) = miners.map(miner => f(miner))
+    val res =
+      ZIO.reduceAllPar(
+        head,
+        tail
+      )((first, second) => first)
+    res
+//      .foldLeft(()) // TODO Consider a cheating opportunity
+//      .reduce(_ race _) // Much terser. I think it's worth using this form
+
+  def findNextBlock(miners: Seq[Miner]) =
+    competeForNextBlock(miners, _.mineBySleeping(10))
+
+  def findNextBlock2(miners: Seq[Miner]) =
+    for
+      startNum <- nextIntBetween(
+        5_000_000,
+        100_000_000
+      ) //This is the value that the prime number finder starts from
+      (miner, completionMessage) <- competeForNextBlockPar(miners, _.mineByCalculatingPrimeNumbers(startNum))
+    yield ((miner, completionMessage))
 
   def run(args: List[String]) = //Use App's run function
     val frop = Miner("Wealthy Frop", gigaFlops = 120)
     val zeb = Miner("Average Zeb", gigaFlops = 100)
     val shtep = Miner("Poor Shtep", gigaFlops = 80)
 
+    val cheep = Miner("Cheatin' cheep", gigaFlops = 150)
+
+    val miners = Seq(cheep, zeb, frop, shtep)
+
     val calculateAndPrintWinner = //Uses mine1 function (Just sleeping)
       for
-        startNum <- nextIntBetween(
-          5_000_000,
-          100_000_000
-        ) //This is the value that the prime number finder starts from
-        raceResult <- findNextBlock2(Seq(zeb, frop, shtep), startNum)
+        raceResult <- findNextBlock2(miners)
         (winner, winnerText) = raceResult
         _ <- putStrLn(winnerText)
       yield (winner)
@@ -54,7 +78,7 @@ object Mining extends zio.App {
     def miningAttempt(
         innards: ZIO[zio.random.Random & zio.Has[
           zio.clock.Clock.Service
-        ] & zio.console.Console, java.io.IOException, Map[Miner, Int]]
+        ] & zio.console.Console, Exception, Map[Miner, Int]]
     ) =
       for
         finalCoinResults: Map[Miner, Int] <- innards
@@ -74,11 +98,11 @@ object Mining extends zio.App {
     val miningWithResults =
       miningAttempt(
         for
-          coinResults <- Ref.make(Map(frop -> 0, zeb -> 0, shtep -> 0))
+          coinResults <- Ref.make(Map(miners.map(_ -> 0):_*))
           _ <-
             (for
-              winner <-
-                calculateAndPrintWinner // TODO Factor out since it exists in the other approach too?
+              // TODO Factor out since it exists in the other approach too?
+              winner <- calculateAndPrintWinner
               currentCoins: Map[Miner, Int] <- coinResults.get
               _ <- coinResults.set(
                 currentCoins.updated(winner, (currentCoins(winner) + 1))
@@ -93,8 +117,8 @@ object Mining extends zio.App {
         ZIO.foldLeft((1 to repetitions))(Map(frop -> 0, zeb -> 0, shtep -> 0)) {
           case (coinResults, _) =>
             for
-              winner <-
-                calculateAndPrintWinner // TODO Factor out since it exists in the other approach too?
+              // TODO Factor out since it exists in the other approach too?
+              winner <- calculateAndPrintWinner
             yield coinResults.updated(winner, (coinResults(winner) + 1))
         }
       )
