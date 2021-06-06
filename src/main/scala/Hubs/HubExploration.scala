e7a3118c8b48550a486a23d4082dcfd19be42d60
bfrasure <bill.frasure@gmail.com>
2021-06-05 18:24:07 -0600

    Save user input to hub and print out, executing both sides repeatedly in parallel #bill #time 20m

--- src/main/scala/Hubs/HubExploration.scala
+++ src/main/scala/Hubs/HubExploration.scala
@@ -4,14 +4,32 @@ object HubExploration extends zio.App {
   import zio._
 
   def run(args: List[String]) = //Use App's run function
+    val getAnInt =
+      for
+        _ <- console.putStrLn("Please provide an int")
+        input <- console.getStrLn
+      yield input.toInt
+
     val logic =
       for
         hub <- Hub.bounded[Int] (2)
         _ <- hub.subscribe.use {
           case hubSubscription =>
+            val getAndStoreInput =
+              for
+                nextInt <- getAnInt
+                _ <- hub.publish(nextInt)
+              yield ()
+
+            val processNextIntAndPrint =
+              for
+                nextInt <- hubSubscription.take
+                _ <- console.putStrLn("Int: " + nextInt)
+              yield ()
+
             for
-              _ <- hub.publish(42)
-              _ <- hubSubscription.take.flatMap(currentInt => console.putStrLn("Int: " + currentInt))
+              _ <- ZIO.collectAllPar(Set(getAndStoreInput, processNextIntAndPrint))
+                    .repeatN(3)
             yield ()
         }
       yield ()
