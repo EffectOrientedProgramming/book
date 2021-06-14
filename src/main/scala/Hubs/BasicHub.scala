ef804bccb73005c7a39786d2638057ce8dd0b34a
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2021-06-14 17:21:05 -0600

    Add to chapters #wyett #time 1h

--- src/main/scala/Hubs/BasicHub.scala
+++ src/main/scala/Hubs/BasicHub.scala
@@ -1,30 +1,28 @@
 package Hubs
+
 import zio.*
 import zio.duration
 import zio.clock
 import zio.console
 
-import java.net.http.HttpResponse.ResponseInfo
 //The purpose of this example to to create a very basic hub that displays small capabilities.
 
-object BasicHub extends zio.App{
-
+object BasicHub extends zio.App {
 
   //This example makes a hub, and publishes a String. Then, two entities take the
   //published string and print it.
   val logic1 =
     Hub.bounded[String](2).flatMap { Hub =>
-      Hub.subscribe.zip(Hub.subscribe).use {
-        case (left,right) =>
-          for
-            _ <- Hub.publish("This is from Hub left!")
-            _ <- left.take.flatMap(console.putStrLn(_))
-            _ <- right.take.flatMap(console.putStrLn(_))
-          yield ()
+      Hub.subscribe.zip(Hub.subscribe).use { case (left, right) =>
+        for
+          _ <- Hub.publish("This is from Hub left!")
+          _ <- left.take.flatMap(console.putStrLn(_))
+          _ <- right.take.flatMap(console.putStrLn(_))
+        yield ()
       }
     }
 
-/*
+  /*
   case class entity(name:String)
   case class question(ques:String)
   case class response(rep:String, ent:entity)
@@ -45,13 +43,9 @@ object BasicHub extends zio.App{
 
 
 
- */
+   */
 
-  def run(args:List[String]) =
-
-  logic1.exitCode
-
-
-
+  def run(args: List[String]) =
+    logic1.exitCode
 
 }
