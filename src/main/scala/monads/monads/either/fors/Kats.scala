11f5b372b03d7cc82f64198cf4f3b06c438157fc
James Ward <james@jamesward.com>
2021-06-23 12:48:25 -0600

    more fun with monads and for comps #wyett #bruce #james #time 4h

---
+++ src/main/scala/monads/monads/either/fors/Kats.scala
@@ -0,0 +1,47 @@
+package monads.monads.either.fors
+
+import scala.util.Random
+
+enum Contents:
+  case Stop()
+  case Continue()
+
+case class Box(contents: Contents):
+  def map(f: Contents.Continue => Contents): Box =
+    contents match
+      case stop: Contents.Stop => this
+      case continue: Contents.Continue => Box(f(continue))
+
+  def flatMap(f: Contents.Continue => Box): Box =
+    contents match
+      case stop: Contents.Stop => this
+      case continue: Contents.Continue => f(continue)
+
+object Box:
+  def observe: Box =
+    if Random.nextBoolean()
+      then Box(Contents.Stop())
+      else Box(Contents.Continue())
+
+def again(continue: Contents.Continue): Box =
+  Box.observe
+
+@main def thefor =
+  val kats = for
+    kat1 <- Box.observe
+    kat2 <- again(kat1)
+    kat3 <- again(kat2)
+  yield kat3 // what we return here has to be a Contents because it must fit in the Box
+
+  println(kats)
+
+@main def unravelled =
+  val kats = Box.observe.flatMap { kat1 =>
+    again(kat1).flatMap { kat2 =>
+      again(kat2).map { kat3 =>
+        kat3
+      }
+    }
+  }
+
+  println(kats)
