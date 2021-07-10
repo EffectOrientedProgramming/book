60bedd8a801f52cc46c54ed98a98709f8df79411
bfrasure <bill.frasure@gmail.com>
2021-07-10 15:59:11 -0600

    Convert WordBag to Operation, and expand error cases #bill #time 45m

---
+++ src/main/scala/monads/Operation.scala
@@ -0,0 +1,58 @@
+package monads
+
+enum Operation:
+
+  def flatMap(f: String => Operation): Operation =
+    this match
+      case _: Bad => this
+      case s: Good =>
+        f(
+          s.content
+        ) // written a different way for illustrating the different syntax options
+
+  def map(f: String => String): Operation =
+    this match
+      case _: Bad        => this
+      case Good(content) => Good(f(content))
+
+  case Bad(reason: String)
+  case Good(content: String)
+
+def httpOperation(content: String, result: String): Operation =
+  if (content.contains("DbResult=Expected"))
+    Operation.Good(content + s" + HttpResult=$result")
+  else
+    Operation.Bad("Did not have required data from DB")
+
+def businessLogic(content: String, result: String): Operation =
+  if (content.contains("HttpResult=Expected"))
+    Operation.Good(content + s" + LogicResult=$result")
+  else
+    Operation.Bad("Did not have required data from Http Call")
+
+@main def happyPath =
+  println(
+    for
+      dbContent <- Operation.Good("DbResult=Expected")
+      httpContent <- httpOperation(dbContent, "Expected")
+      logicContent <- businessLogic(httpContent, "Expected")
+    yield logicContent
+  )
+
+@main def sadPathDb =
+  println(
+    for
+      dbContent <- Operation.Good("DbResult=Unexpected")
+      httpContent <- httpOperation(dbContent, "Expected")
+      logicContent <- businessLogic(httpContent, "Expected")
+    yield logicContent
+  )
+
+@main def sadPathHttp =
+  println(
+    for
+      dbContent <- Operation.Good("DbResult=Expected")
+      httpContent <- httpOperation(dbContent, "Unexpected")
+      logicContent <- businessLogic(httpContent, "Expected")
+    yield logicContent
+  )
