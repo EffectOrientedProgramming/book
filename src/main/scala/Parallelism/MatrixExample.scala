e14c799b3027407cffaef2b935b158994de222a4
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2021-07-14 11:14:08 -0600

    Add folding examples #wyett #time 1h

--- src/main/scala/Parallelism/MatrixExample.scala
+++ src/main/scala/Parallelism/MatrixExample.scala
@@ -1,48 +1,40 @@
 package Parallelism
+
 import zio.*
 
 object MatrixExample {
 
-  case class matrix2D(sizeX:Int, sizeY:Int, twoDList:List[List[Int]]) :
-      def mapInt(f: Int => Int):matrix2D =
-        val newData = this.twoDList.map(i =>
-          i.map(j =>
-            f(j)
-          )
-        )
-        matrix2D(this.sizeX, this.sizeY, newData)
+  case class matrix2D(sizeX: Int, sizeY: Int, twoDList: List[List[Int]]):
+    def mapInt(f: Int => Int): matrix2D =
+      val newData = this.twoDList.map(i => i.map(j => f(j)))
+      matrix2D(this.sizeX, this.sizeY, newData)
 
-      def printMat():Unit = //Uses the map function to itterate
-        this.mapInt(i =>
-          println(i)
-          i
-        )
+    def printMat(): Unit = //Uses the map function to itterate
+      this.mapInt(i =>
+        println(i)
+        i
+      )
 
+    def map(f: => Int): matrix2D =
+      this
 
-      def map(f: => Int):matrix2D =
-        this
-
-  def printMat(mat:matrix2D):Unit =
+  def printMat(mat: matrix2D): Unit =
     mat.twoDList.foreach(i =>
-      i.foreach(j =>
-        print(s"  $j  "))
+      i.foreach(j => print(s"  $j  "))
       println("\n")
     )
 
-
-  def makeMatrix2D(sizeX:Int, sizeY:Int):matrix2D =
-    val rows:List[Int] = List.range(0, sizeY)
+  def makeMatrix2D(sizeX: Int, sizeY: Int): matrix2D =
+    val rows: List[Int] = List.range(0, sizeY)
     //println(rows)
-    val matr = rows.map(i => List.range(i*sizeX, i*sizeX + sizeX))
+    val matr = rows.map(i => List.range(i * sizeX, i * sizeX + sizeX))
     //println(matr)
     matrix2D(sizeX, sizeY, matr)
 
-  def flipMatrix(matr:matrix2D):matrix2D =
+  def flipMatrix(matr: matrix2D): matrix2D =
     def flippedCol(col: List[Int]) =
-      col.foldLeft(List(col(0)))( (a,i) =>
-        a.::(i)
-      )
-    val flippedMat = matr.twoDList.foldLeft(List(List.empty[Int]))( (b,j) =>
+      col.foldLeft(List(col(0)))((a, i) => a.::(i))
+    val flippedMat = matr.twoDList.foldLeft(List(List.empty[Int]))((b, j) =>
       b.::(flippedCol(j).dropRight(1))
     )
     matrix2D(matr.sizeX, matr.sizeY, flippedMat)
@@ -51,7 +43,7 @@ object MatrixExample {
 
 @main def matricies =
   val mEx = MatrixExample
-  val matrix = mEx.makeMatrix2D(4,4)
+  val matrix = mEx.makeMatrix2D(4, 4)
   println("Normal Matrix:")
   mEx.printMat(matrix)
   //matrix.printMat()
@@ -60,13 +52,13 @@ object MatrixExample {
   mEx.printMat(flippedMat)
   //flippedMat.printMat()
 
-  val bigMat = mEx.makeMatrix2D(6000,6000) //from clean, takes around 15 sec
+  val bigMat = mEx.makeMatrix2D(6000, 6000) //from clean, takes around 15 sec
   //mEx.printMat(bigMat)
-  val zbigMat = ZIO{ bigMat}
+  val zbigMat = ZIO { bigMat }
   val logic =
     for
-     flattened:mEx.matrix2D <- zbigMat
-     zbigger:mEx.matrix2D <- ZIO{flattened.mapInt(i => i + 1)}
+      flattened: mEx.matrix2D <- zbigMat
+      zbigger: mEx.matrix2D <- ZIO { flattened.mapInt(i => i + 1) }
     yield zbigger
 
   logic.exitCode
