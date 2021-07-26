7e5992754e634842ec62be55b5e759250f4767c4
bfrasure <bill.frasure@gmail.com>
2021-07-25 23:02:12 -0600

    Grapple with generic SensorData in layers/Has #bill #time 1h

--- src/main/scala/scenarios/SecuritySystem.scala
+++ src/main/scala/scenarios/SecuritySystem.scala
@@ -8,6 +8,14 @@ import zio.duration.durationInt
 import zio.Schedule
 import scala.concurrent.TimeoutException
 
+case class TempSense(
+    z: ZIO[
+      zio.clock.Clock,
+      HardwareFailure,
+      ZIO[Clock, TimeoutException, Degrees]
+    ]
+)
+
 /** Situations: Security System: Should monitor
   *   - Motion
   *   - Heat/Infrared
@@ -16,14 +24,17 @@ import scala.concurrent.TimeoutException
   *   - Loud Local Siren
   *   - Ping security company
   *   - Notify police
-  *
-  * TODO Investigate services that enable timed,
-  * chunked results. eg:
-  *   - 70 degrees for 5 seconds
-  *   - 71 degrees for 10 seconds
-  *   - 98 degrees for 3 seconds
   */
 object SecuritySystem:
+  // TODO Why can't I use this???
+  val s: zio.ZLayer[Any, Nothing, zio.Has[
+    scenarios.TempSense
+  ]] =
+    SensorData.live[Degrees, TempSense](
+      x => TempSense(x),
+      (1.seconds, Degrees(71)),
+      (2.seconds, Degrees(70))
+    )
 
   val fullLayer: ZLayer[Any, Nothing, zio.Has[
     scenarios.MotionDetector.ServiceX
@@ -32,7 +43,8 @@ object SecuritySystem:
       ThermalDetector.live(
         (1.seconds, Degrees(71)),
         (2.seconds, Degrees(70))
-      ) ++ Siren.live
+      ) ++ Siren.live // ++ s
+  end fullLayer
 
   val x: zio.ZIO[
     scenarios.MotionDetector.ServiceX,
@@ -40,61 +52,76 @@ object SecuritySystem:
     scenarios.Pixels
   ] = ZIO.accessM(_.amountOfMotion())
 
-  val y: zio.ZIO[Has[
+  val accessMotionDetector: zio.ZIO[Has[
     scenarios.MotionDetector.ServiceX
   ], scenarios.HardwareFailure, scenarios.Pixels] =
     ZIO.accessM(_.get.amountOfMotion())
+
+  val accessThermalDetectorX: zio.ZIO[zio.Has[
+    scenarios.ThermalDetector.Service
+  ] & zio.clock.Clock, scenarios.HardwareFailure, zio.ZIO[
+    zio.clock.Clock,
+    scala.concurrent.TimeoutException,
+    scenarios.Degrees
+  ]] =
+    ZIO.accessM[Has[
+      scenarios.ThermalDetector.Service
+    ] & Clock](
+      _.get[scenarios.ThermalDetector.Service]
+        .amountOfHeat()
+    )
+
+  def securityLoop(
+      amountOfHeatGenerator: zio.ZIO[
+        zio.clock.Clock,
+        scala.concurrent.TimeoutException,
+        scenarios.Degrees
+      ],
+      amountOfMotion: Pixels,
+      siren: Siren.ServiceX
+  ): ZIO[
+    zio.clock.Clock,
+    scala.concurrent.TimeoutException |
+      HardwareFailure,
+    Unit
+  ] =
+    for
+      amountOfHeat <- amountOfHeatGenerator
+      _ <-
+        zprintln(
+          s"Heat: $amountOfHeat  Motion: $amountOfMotion"
+        )
+      _ <-
+        if shouldTrigger(
+            amountOfMotion,
+            amountOfHeat
+          )
+        then
+          siren.lowBeep()
+        else
+          zprintln("No need to panic")
+    yield ()
 
   def shouldAlertServices(): ZIO[Has[
     MotionDetector.ServiceX
   ] & Has[ThermalDetector.Service] & Has[Siren.ServiceX] & Clock, scenarios.HardwareFailure | TimeoutException, String] =
     ZIO
-      .services[
-        MotionDetector.ServiceX,
-        ThermalDetector.Service,
-        Siren.ServiceX
-      ]
-      .flatMap {
-        (
-            motionDetector,
-            thermalDetector,
-            siren
-        ) =>
-          for
-            amountOfMotion <-
-              motionDetector.amountOfMotion()
-            amountOfHeatGenerator <-
-              thermalDetector.amountOfHeat()
-            _ <-
-              (
-                for
-                  amountOfHeat <-
-                    amountOfHeatGenerator
-                  _ <-
-                    zprintln(
-                      s"Heat: $amountOfHeat  Motion: $amountOfMotion"
-                    )
-                  _ <-
-                    if shouldTrigger(
-                        amountOfMotion,
-                        amountOfHeat
-                      )
-                    then
-                      siren.lowBeep()
-                    else
-                      zprintln(
-                        "No need to panic"
-                      )
-                yield ()
-              ).catchAll { case _ =>
-                  ZIO
-                    .fail(new HardwareFailure {})
-                }
-                .repeat(
-                  Schedule.recurs(3) &&
-                    Schedule.spaced(500.millis)
-                )
-          yield "Fin"
+      .service[Siren.ServiceX]
+      .flatMap { siren =>
+        for
+          amountOfMotion <- accessMotionDetector
+          amountOfHeatGenerator <-
+            accessThermalDetectorX
+          _ <-
+            securityLoop(
+              amountOfHeatGenerator,
+              amountOfMotion,
+              siren
+            ).repeat(
+              Schedule.recurs(3) &&
+                Schedule.spaced(500.millis)
+            )
+        yield "Fin"
       }
 
   def shouldTrigger(
@@ -201,3 +228,23 @@ def zprintln(
     output: String
 ): ZIO[Any, scenarios.HardwareFailure, Unit] =
   ZIO.succeed(println(output))
+
+// TODO Figure out how to use this
+object SensorData:
+  def live[T, Y](
+      c: ZIO[
+        zio.clock.Clock,
+        HardwareFailure,
+        ZIO[Clock, TimeoutException, T]
+      ] => Y,
+      value: (Duration, T),
+      values: (Duration, T)*
+  ): ZLayer[Any, Nothing, Has[Y]] =
+    ZLayer.succeed(
+      // that same service we wrote above
+      c(
+        Scheduled2
+          .scheduledValues[T](value, values*)
+      )
+    )
+end SensorData
