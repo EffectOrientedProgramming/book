3371e6569a89a285cd772e4c6425ef19ddbd029f
Bruce Eckel <BruceTEckel@gmail.com>
2021-07-29 13:56:10 -0600

    Added makeYield and letter ids #bruce #time 30m
    Added makeYield and letter ids

--- src/main/scala/monads/Result.scala
+++ src/main/scala/monads/Result.scala
@@ -3,30 +3,36 @@ package monads
 
 // More illumination but not more reader clarity...
 
-// TODO: Show yield expression failing, for completeness
-
-def test(n: Int) =
+def test(n: Char) =
   println(s">> test $n <<")
 
-  def produceResult(id: Int) =
-    val msg = s"n:$n|id:$id"
+  def makeResult(id: Char): Result[String, String] =
+    val msg = s"$n|$id"
     val result = if n == id
       then Fail(msg)
       else Succeed(msg)
-    println(s"produceResult($id): $result")
+    println(s"makeResult($id): $result")
+    result
+
+  def makeYield(id: Char, i: String, j: String, k: String): Result[String, String] =
+    val msg = s"$n|$id, $i, $j $k"
+    val result = if n == id
+      then Fail(msg)
+      else Succeed(msg)
+    println(s"makeYield($id): $result")
     result
 
   val yielded = for
-    i: String <- produceResult(0)
-    j: String <- produceResult(1)
-    k: String <- produceResult(2)
-  yield (i, j, k)
+    i: String <- makeResult('a')
+    j: String <- makeResult('b')
+    k: String <- makeResult('c')
+  yield makeYield('d', i, j, k)
 
   println(s"test($n) produced $yielded")
 
 @main
 def results =
-  0 to 3 map test
+  'a' to 'e' map test
 
 // Shown later in the chapter:
 
@@ -36,7 +42,7 @@ trait Result[+E, +A]:
     val fm = this.match
       case Succeed(a) => f(a)
       case fail: Fail[E] => fail
-    println(s"flatMap() on $this produces $fm")
+    println(s"flatMap() on $this: $fm")
     fm
 
   def map[B](f: A => B): Result[E, B] =
@@ -44,7 +50,7 @@ trait Result[+E, +A]:
     val m = this.match
       case Succeed(a) => Succeed(f(a))
       case fail: Fail[E] => fail
-    println(s"map() on $this produces $m")
+    println(s"map() on $this: $m")
     m
 
 case class Fail[+E](fail: E) extends Result[E, Nothing]
