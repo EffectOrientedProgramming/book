ea87f96a173034ddc46c2af65bf587cd22534673
Bruce Eckel <BruceTEckel@gmail.com>
2021-07-29 13:25:29 -0600

    Result.scala #bruce #time 1h
    More illumination but not more reader clarity

--- src/main/scala/monads/Result.scala
+++ src/main/scala/monads/Result.scala
@@ -1,47 +1,53 @@
 // Monads/Result.scala
 package monads
 
-// Much closer but not finished yet...
+// More illumination but not more reader clarity...
+
+// TODO: Show yield expression failing, for completeness
 
 def test(n: Int) =
   println(s">> test $n <<")
 
   def produceResult(id: Int) =
+    val msg = s"n:$n|id:$id"
     val result = if n == id
-      then Fail(s"n:$n, id:$id")
-      else Success(s"n:$n, id:$id")
+      then Fail(msg)
+      else Succeed(msg)
     println(s"produceResult($id): $result")
     result
 
-  // TODO: Figure out how to use intermediate results meaningfully:
   val yielded = for
-    i <- produceResult(0)
-    j <- produceResult(1)
-    k <- produceResult(2)
-  yield produceResult(3)
+    i: String <- produceResult(0)
+    j: String <- produceResult(1)
+    k: String <- produceResult(2)
+  yield (i, j, k)
 
   println(s"test($n) produced $yielded")
 
 @main
-def resultEssence =
-  0 to 4 map test
+def results =
+  0 to 3 map test
 
 // Shown later in the chapter:
 
 trait Result[+E, +A]:
   def flatMap[E1, B](f: A => Result[E1, B]): Result[E | E1, B] =
-    println("flatMap()")
-    this.match
-      case Success(a) => f(a)
+    println(s"flatMap() on $this")
+    val fm = this.match
+      case Succeed(a) => f(a)
       case fail: Fail[E] => fail
+    println(s"flatMap() on $this produces $fm")
+    fm
 
   def map[B](f: A => B): Result[E, B] =
-    println("map()")
-    this.match
-      case Success(a) => Success(f(a))
+    println(s"map() on $this")
+    val m = this.match
+      case Succeed(a) => Succeed(f(a))
       case fail: Fail[E] => fail
+    println(s"map() on $this produces $m")
+    m
 
 case class Fail[+E](fail: E) extends Result[E, Nothing]
-case class Success[+A](succeed: A) extends Result[Nothing, A]
+case class Succeed[+A](succeed: A) extends Result[Nothing, A]
 
 // TODO: Can Result be further simplified?
