3bee2308deeeb4a462719dd991709521276811c9
Bruce Eckel <BruceTEckel@gmail.com>
2021-07-31 10:17:14 -0600

    Update TestStringResult.scala

--- src/main/scala/monads/TestStringResult.scala
+++ src/main/scala/monads/TestStringResult.scala
@@ -20,17 +20,10 @@ def test(n: Char) =
       b: String <- op('b')
       c: String <- op('c')
     yield
-      val msg = s"$n|d, $a, $b, $c"
-      val result: Result =
-        if n == 'd' then
-          Fail(msg)
-        else
-          Success(msg)
-      println(s"yielding $result")
-      result.toString
+      s"Completed yield: $n|d, a:$a, b:$b, c:$c"
 
   println(s"test($n): $test")
 end test
 
 @main
-def results = 'a' to 'e' map test
+def results = 'a' to 'd' map test
