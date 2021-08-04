68ba81c370a54de79924b375271c64bf622f4b97
bfrasure <bill.frasure@gmail.com>
2021-08-04 10:34:55 -0600

    Break down ScheduledValues code #bill 25m

--- src/main/scala/scenarios/ScheduledValues.scala
+++ src/main/scala/scenarios/ScheduledValues.scala
@@ -15,48 +15,10 @@ import scala.concurrent.TimeoutException
 /* Goal: If I accessed this from:
  * 0-1 seconds, I would get "First Value" 1-4
  * seconds, I would get "Second Value" 4-14
- * seconds, I would get "Third Value" 14+
+ * seconds, I would get "Third Value" 14+ *
  * seconds, it would fail */
 
-object ScheduledValues:
-
-  def scheduledValues[T](
-      list: List[(Duration, T)]
-  ): ZIO[Has[Clock], TimeoutException, T] = ???
-
-  scheduledValues(
-    List(
-      (
-        Duration(1, TimeUnit.SECONDS),
-        "First Value"
-      ),
-      (
-        Duration(3, TimeUnit.SECONDS),
-        "Second Value"
-      ),
-      (
-        Duration(10, TimeUnit.SECONDS),
-        "Third Value"
-      )
-    )
-  )
-end ScheduledValues
-
 object Scheduled2:
-
-  private def access[A](
-      timeTable: Seq[(Long, A)]
-  ): ZIO[Has[Clock], TimeoutException, A] =
-    for
-      clock <- ZIO.environment[Has[Clock]]
-      now   <- clock.get[Clock].nanoTime
-      result <-
-        ZIO.getOrFailWith(
-          new TimeoutException("TOO LATE")
-        ) {
-          timeTable.find(_._1 >= now).map(_._2)
-        }
-    yield result
 
   def scheduledValues[A](
       value: (Duration, A),
@@ -65,21 +27,39 @@ object Scheduled2:
     Clock
   ], TimeoutException, A]] =
     for
-      clock     <- ZIO.environment[Has[Clock]]
-      startTime <- clock.get[Clock].nanoTime
+      startTime: Long <- Clock.nanoTime
       timeTable =
-        values.scanLeft(
-          (
-            startTime + value._1.toNanos,
-            value._2
-          )
-        ) {
-          case (
-                (elapsed, _),
-                (duration, value)
-              ) =>
-            (elapsed + duration.toNanos, value)
-        }
+        createTimeTable(
+          startTime,
+          value,
+          values* // Yay Scala3 :)
+        )
     yield access(timeTable.toList)
+
+  // TODO Some comments, tests, examples, etc to
+  // make this function more obvious
+  private def createTimeTable[A](
+      startTime: Long,
+      value: (Duration, A),
+      values: (Duration, A)*
+  ) =
+    values.scanLeft(
+      (startTime + value._1.toNanos, value._2)
+    ) { case ((elapsed, _), (duration, value)) =>
+      (elapsed + duration.toNanos, value)
+    }
+
+  private def access[A](
+      timeTable: Seq[(Long, A)]
+  ): ZIO[Has[Clock], TimeoutException, A] =
+    for
+      now <- Clock.nanoTime
+      result <-
+        ZIO.getOrFailWith(
+          new TimeoutException("TOO LATE")
+        ) {
+          timeTable.find(_._1 >= now).map(_._2)
+        }
+    yield result
 
 end Scheduled2
