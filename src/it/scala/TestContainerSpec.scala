24d66b25caf2e37bfafa8b74e619c87f09a1d450
Bill Frasure <bill.frasure@gmail.com>
2021-09-30 21:03:05 -0600

    Ensure containers are stopped/removed after test #bill #time 30m

--- src/it/scala/TestContainerSpec.scala
+++ src/it/scala/TestContainerSpec.scala
@@ -16,40 +16,60 @@ import org.testcontainers.containers.{
 
 import io.getquill._
 
-object MyApp {
-  def quillStuff(exposedPort: Int, username: String, password: String) =
-    case class Person(firstName: String, lastName: String, age: Int)
+object MyApp:
+  def quillStuff(
+      exposedPort: Int,
+      username: String,
+      password: String
+  ) =
+    case class Person(
+        firstName: String,
+        lastName: String,
+        age: Int
+    )
 
-    import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
-    val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
+    import com.zaxxer.hikari.{
+      HikariConfig,
+      HikariDataSource
+    }
+    val pgDataSource =
+      new org.postgresql.ds.PGSimpleDataSource()
     pgDataSource.setUser(username)
-    pgDataSource.setPortNumbers(Array(exposedPort))
+    pgDataSource
+      .setPortNumbers(Array(exposedPort))
     pgDataSource.setPassword(password)
     val config = new HikariConfig()
     config.setDataSource(pgDataSource)
-    val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(config))
+    val ctx =
+      new PostgresJdbcContext(
+        LowerCase,
+        new HikariDataSource(config)
+      )
 
     // SnakeCase turns firstName -> first_name
-    // val ctx = new PostgresJdbcContext(SnakeCase, "")
+    // val ctx = new
+    // PostgresJdbcContext(SnakeCase, "")
     import ctx._
 
     val named = "Joe"
-    inline def somePeople = quote {
-      query[Person].filter(p => p.firstName == lift(named))
-    }
+    inline def somePeople =
+      quote {
+        query[Person].filter(p =>
+          p.firstName == lift(named)
+        )
+      }
     val people: List[Person] = run(somePeople)
     // TODO Get SQL
     println(people)
-}
-
+  end quillStuff
+end MyApp
 
 object ManagedTestInstances:
-  val network =
-    ZManaged
-      .acquireReleaseWith(
-        ZIO.succeed(Network.newNetwork().nn)
-      )(n => ZIO.succeed(n.close))
-      .useNow
+    
+
+end ManagedTestInstances
+
+// TODO Figure out if TESTCONTAINERS_RYUK_DISABLED=true is a band-aid that's avoiding the real problem with test cleanup
 
 object TestContainersSpec
     extends DefaultRunnableSpec:
@@ -60,32 +80,51 @@ object TestContainersSpec
         "Intercept and format MatchError from unhandled RuntimeException"
       ) {
         for
-          network <- ManagedTestInstances.network
-          safePostgres <-
-            PostgresContainer
-              .construct("init.sql")
-              .provideSomeLayer(
-                ZLayer.succeed(network)
-              )
-          _ <- ZIO.succeed(safePostgres.start)
-          _ <-
-            mdoc.wrapUnsafeZIO(
-              ZIO.succeed(
-                throw new MatchError(
-                  MdocSession.App.GpsException()
-                )
-              )
-            )
+          _ <- 
+            ZIO
+              .acquireReleaseWith(
+                ZIO.succeed(Network.newNetwork().nn), (n: Network) =>  ZIO.debug("Trying to close network..."), {
+
+                  (network: Network) => 
+                    for {
+                      safePostgres <-
+                        PostgresContainer
+                          .construct("init.sql")
+                          .provideSomeLayer(
+                            ZLayer.succeed(network)
+                          )
+                      _ <- ZIO.succeed(safePostgres.start)
+                      _ <-
+                        mdoc.wrapUnsafeZIO(
+                          ZIO.succeed(
+                            throw new MatchError(
+                              MdocSession.App.GpsException()
+                            )
+                          )
+                        )
+                      _ <-
+                        ZIO.attempt {
+                          println(
+                            "Bound port numbers: " +
+                              safePostgres
+                                .getMappedPort(5432)
+                          )
+                          println(
+                            "Exposed ports: " +
+                              safePostgres.getExposedPorts
+                          )
+                          MyApp.quillStuff(
+                            safePostgres
+                              .getMappedPort(5432)
+                              .nn,
+                            safePostgres.getUsername.nn,
+                            safePostgres.getPassword.nn
+                          )
+                        }
+                      _ <- ZIO.succeed(safePostgres.close)
+                    } yield ()
+                })
           output <- TestConsole.output
-          _ <- ZIO.attempt {
-            println("Bound port numbers: " + safePostgres.getMappedPort(5432))
-            println("Exposed ports: " + safePostgres.getExposedPorts)
-            MyApp.quillStuff(safePostgres.getMappedPort(5432).nn, safePostgres.getUsername.nn, safePostgres.getPassword.nn)
-          }
-          _ <-
-            printLine("With managed Network 1")
-          _ <- ZIO.succeed(safePostgres.close)
-          _ <- ZIO.succeed(network.close)
         yield assert(output)(
           equalTo(
             Vector(
