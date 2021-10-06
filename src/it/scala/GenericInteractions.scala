3bf9e6fe0e1f89501b572f4a04c65c4fa0a7e8c1
Bill Frasure <bill.frasure@gmail.com>
2021-10-06 14:34:27 -0600

    Delete _most_ of GenericInteractions. This is so, so weird #bill #time 15m

--- src/it/scala/GenericInteractions.scala
+++ src/it/scala/GenericInteractions.scala
@@ -1,58 +1,9 @@
 package mdoc
 
-import zio.*
-import org.testcontainers.containers.{
-  GenericContainer,
-}
+import org.testcontainers.containers.GenericContainer
 
 object GenericInteractions:
-  def manage[T <: GenericContainer[T]](
-      c: T,
-      containerType: String
-  ) =
-    ZManaged.acquireReleaseWith(
-      ZIO.debug(s"Creating $containerType") *>
-        start(c, containerType) *> ZIO.succeed(c)
-    )((n: T) =>
-      ZIO.attempt(n.close()).orDie *>
-        ZIO.debug(s"Closing $containerType")
-    )
 
-  def manageWithInitialization[
+  def stupidFuckingMethodThatICannotDelete[
       T <: GenericContainer[T]
-  ](
-      c: T,
-      containerType: String,
-      initialize: T => ZIO[
-        Any,
-        Throwable,
-        Unit
-      ] = (_: T) => ZIO.unit
-  ) =
-    ZManaged.acquireReleaseWith(
-      ZIO.debug(s"Creating $containerType") *>
-        start(c, containerType) *>
-        initialize(c) *> ZIO.succeed(c)
-    )((n: T) =>
-      ZIO.attempt(n.close()).orDie *>
-        ZIO.debug(s"Closing $containerType")
-    )
-
-  private def start[T <: GenericContainer[T]](
-      c: T,
-      containerType: String
-  ) =
-    ZIO.blocking(ZIO.succeed(c.start)) *>
-      ZIO.debug(
-        s"Finished blocking for $containerType container creation"
-      )
-
-end GenericInteractions
-
-import zio.*
-import org.testcontainers.containers.{
-  GenericContainer,
-}
-
-
-
+  ]() = ???
