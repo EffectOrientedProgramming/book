4e10febe69be5d07d66e10a64e3ceb2d8506f86a
Bill Frasure <bill.frasure@gmail.com>
2022-01-06 21:15:13 -0700

    Exploring more ZIO from scratch #bill #bruce #time 1h

---
+++ experiments/src/main/scala/ZIOFromNothing/XEnvironment.scala
@@ -0,0 +1,9 @@
+package ZIOFromNothing
+
+class XEnvironment():
+    def increment(y: Int): Int = 
+        XEnvironment.x += y
+        XEnvironment.x 
+
+object XEnvironment:
+  private var x: Int = 0
