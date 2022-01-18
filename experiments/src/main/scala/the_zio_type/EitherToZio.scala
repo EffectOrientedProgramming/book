76b64fae7e8c8d674ad096e9716f446512014a20
Bill Frasure <bill.frasure@gmail.com>
2022-01-17 21:51:26 -0700

    Condense EitherToZio example

--- experiments/src/main/scala/the_zio_type/EitherToZio.scala
+++ experiments/src/main/scala/the_zio_type/EitherToZio.scala
@@ -6,23 +6,19 @@ import zio._
 import java.io
 import java.io.IOException
 
-class EitherToZio:
-  // Depending on the input, sEither can be a
-  // String or an Int
-  val input = "I am a string"
+case class InvalidIntegerInput(value: String) 
 
-  val sEither: Either[String, Int] =
-    try
-      Right(
-        input.toInt
-      ) // Right case is an integer
-    catch
-      case e: NumberFormatException =>
-        Left(input) // Left case is an error type
+def parseInteger(input: String): Either[InvalidIntegerInput, Int] =
+  try
+    Right(
+      input.toInt
+    ) // Right case is an integer
+  catch
+    case e: NumberFormatException =>
+      Left(InvalidIntegerInput(input)) // Left case is an error type
+object EitherToZio extends ZIOAppDefault:
 
-  // We can translate this directly into a more
-  // succinct and readable IO.
+  val zEither: IO[InvalidIntegerInput, Int] =
+    ZIO.fromEither(parseInteger("Not an integer"))
 
-  val zEither: IO[String, Int] =
-    ZIO.fromEither(sEither)
-end EitherToZio
+  def run = zEither
