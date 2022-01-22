0c904e94b06f0f1d8b619dcbc760df87eb70052f
Bill Frasure <bill.frasure@gmail.com>
2022-01-22 16:18:42 -0700

    rm default RandomBoundedInt impl #time #10m #bill

--- experiments/src/main/scala/random/OldUnsafeExamples.scala
+++ experiments/src/main/scala/random/OldUnsafeExamples.scala
@@ -8,31 +8,30 @@ import scala.util.Random
 // Anything that has a randomly generated
 // component is an effect
 
-def rollDice(): Int =
-  Random.nextInt(6) + 1
+def rollDice(): Int = Random.nextInt(6) + 1
 
 @main
 def randNumEx =
   println(rollDice())
   println(rollDice())
 
-
 def fullRound(): String =
   val roll1 = rollDice()
   val roll2 = rollDice()
 
-  (roll1, roll2) match {
-    case (6, 6) => "Jackpot! Winner!"
-    case (1, 1) => "Snake eyes! Loser!"
+  (roll1, roll2) match
+    case (6, 6) =>
+      "Jackpot! Winner!"
+    case (1, 1) =>
+      "Snake eyes! Loser!"
     case (_, _) =>
       "Nothing interesting. Try again."
-  }
 
 @main
-def playUntilWinOrLoss() =
-  println(fullRound())
+def playUntilWinOrLoss() = println(fullRound())
 
-def rollDiceZ(): ZIO[RandomBoundedInt, Nothing, Int] =
+def rollDiceZ()
+    : ZIO[RandomBoundedInt, Nothing, Int] =
   RandomBoundedInt.nextIntBetween(1, 7)
 
 import zio.ZIOApp
@@ -41,27 +40,30 @@ import zio.{Tag, UIO, ZEnv, ZIOAppArgs, ZLayer}
 object randNumExZ extends ZIOApp:
   type Environment = RandomBoundedInt
 
-  val layer: ZLayer[ZIOAppArgs, Any, RandomBoundedInt] = RandomBoundedInt.live
+  val layer: ZLayer[
+    ZIOAppArgs,
+    Any,
+    RandomBoundedInt
+  ] = RandomBoundedInt.live
 
-  val tag: Tag[RandomBoundedInt] = Tag[RandomBoundedInt]
+  val tag: Tag[RandomBoundedInt] =
+    Tag[RandomBoundedInt]
   def run =
     for
       roll1 <- rollDiceZ()
       roll2 <- rollDiceZ()
-      _ <- ZIO.debug(roll1)
-      _ <- ZIO.debug(roll2)
+      _     <- ZIO.debug(roll1)
+      _     <- ZIO.debug(roll2)
     yield ()
-
-
-
 
 def fullRoundZ(): String =
   val roll1 = rollDice()
   val roll2 = rollDice()
 
-  (roll1, roll2) match {
-    case (6, 6) => "Jackpot! Winner!"
-    case (1, 1) => "Snake eyes! Loser!"
+  (roll1, roll2) match
+    case (6, 6) =>
+      "Jackpot! Winner!"
+    case (1, 1) =>
+      "Snake eyes! Loser!"
     case (_, _) =>
       "Nothing interesting. Try again."
-  }
--- experiments/src/main/scala/random/RandomBoundedInt.scala
+++ experiments/src/main/scala/random/RandomBoundedInt.scala
@@ -3,13 +3,11 @@ package random
 import zio.{Tag, UIO, ZEnv, ZIO, ZIOAppArgs}
 import scala.util.Random
 
-
 trait RandomBoundedInt:
   def nextIntBetween(
-                      minInclusive: Int,
-                      maxExclusive: Int
-                    ): UIO[Int] =
-    ZIO.succeed(Random.between(minInclusive, maxExclusive))
+      minInclusive: Int,
+      maxExclusive: Int
+  ): UIO[Int]
 
 import zio.{UIO, ZIO, ZLayer}
 
@@ -17,15 +15,29 @@ import scala.util.Random
 
 object RandomBoundedInt:
   def nextIntBetween(
-                      minInclusive: Int,
-                      maxExclusive: Int
-                    ): ZIO[RandomBoundedInt, Nothing, Int]  =
-    ZIO.serviceWithZIO[RandomBoundedInt](_.nextIntBetween(minInclusive, maxExclusive))
+      minInclusive: Int,
+      maxExclusive: Int
+  ): ZIO[RandomBoundedInt, Nothing, Int] =
+    ZIO.serviceWithZIO[RandomBoundedInt](
+      _.nextIntBetween(
+        minInclusive,
+        maxExclusive
+      )
+    )
 
-  val live: ZLayer[Any, Nothing, RandomBoundedInt] = ZLayer.succeed(new RandomBoundedInt {
-    override def nextIntBetween(
-                        minInclusive: Int,
-                        maxExclusive: Int
-                      ): UIO[Int] =
-      ZIO.succeed(Random.between(minInclusive, maxExclusive))
-  })
+  val live
+      : ZLayer[Any, Nothing, RandomBoundedInt] =
+    ZLayer.succeed(
+      new RandomBoundedInt:
+        override def nextIntBetween(
+            minInclusive: Int,
+            maxExclusive: Int
+        ): UIO[Int] =
+          ZIO.succeed(
+            Random.between(
+              minInclusive,
+              maxExclusive
+            )
+          )
+    )
+end RandomBoundedInt
