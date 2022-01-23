0ffbd27a9d83a8545ebacbe1526835aa2d1728a2
Bill Frasure <bill.frasure@gmail.com>
2022-01-23 14:50:58 -0700

    Cleanup Equality and Alias experiments #time #15m #bill

--- experiments/src/main/scala/zioBasics/Alias.scala
+++ experiments/src/main/scala/zioBasics/Alias.scala
@@ -1,19 +1,8 @@
 package zioBasics
 
-import java.io
 import zio._
-import java.io.IOException
 
 object Alias:
-  // General Alias Table:
-  // UIO[A] = ZIO[Any, Nothing, A] Any
-  // environment, No Errors
-  // URIO[R,A] = ZIO[R, Nothing, A]    No errors
-  // Task[A] = ZIO[Any, Throwable, A] Any
-  // environment, Throwable errors
-  // RIO[A] = ZIO[R, Throwable, A] Throwable
-  // Errors
-  // IO[E,A] = ZIO[Any, E, A] Any Environment
 
   // suc(1-5) are all equivalant.
   // Their types can be aliased to be more and
@@ -25,4 +14,3 @@ object Alias:
   val suc3: IO[Nothing, Int] = ZIO.succeed(1)
   val suc4: URIO[Any, Int]   = ZIO.succeed(1)
   val suc5: UIO[Int]         = ZIO.succeed(1)
-end Alias
--- experiments/src/main/scala/zioBasics/Equality.scala
+++ experiments/src/main/scala/zioBasics/Equality.scala
@@ -1,78 +1,51 @@
 package zioBasics
 
-import java.io
-import zio._
-import zio.Console._
+import zio.*
+import zio.Console.printLine
 
 import java.io.IOException
 
-object Equality extends zio.App:
+val suc1 = ZIO.succeed(1)
+val suc2: ZIO[Any, Nothing, Int] =
+  ZIO.succeed(1)
+val suc3: IO[Nothing, Int] = ZIO.succeed(1)
 
-  // Equality may be non-intuative when it comes
-  // to ZIO.
-  // suc(1-5) are all equivalant. They all equal
-  // a success with a return of 1.
-  // (Their types can be aliased to be more and
-  // more specific.)
-  val suc1 = ZIO.succeed(1)
+val suc4: UIO[Int]         = ZIO.succeed(1)
+val suc4duplicate: UIO[Int] =
+  ZIO.succeed(1)
 
-  val suc2: ZIO[Any, Nothing, Int] =
-    ZIO.succeed(1)
-  val suc3: IO[Nothing, Int] = ZIO.succeed(1)
-  val suc4: UIO[Int]         = ZIO.succeed(1)
+val suc5: URIO[Any, Int] = ZIO.succeed(1)
 
-  val suc4d: UIO[Int] =
-    ZIO.succeed(1) // (Duplicate of suc4)
-  val suc5: URIO[Any, Int] = ZIO.succeed(1)
-
-  // Here, we test the equality values of all the
-  // ZIO:
-  val myAppLogic
-      : ZIO[Console, IOException, Unit] =
-    for
-      res1: Int <-
-        suc1 // Flat map all the ZIO into their integer values
-      res2: Int <- suc2
-      res3: Int <- suc3
-      res4: Int <- suc4
-      res5: Int <- suc5
-      _ <-
-        printLine(s"""
-                       | res1: ${res1}
-                       | res2: ${res2}
-                       | res3: ${res3}
-                       | res4: ${res4}
-                       | res5: ${res5}
-                       | """.stripMargin)
-      // Test if the flat mapped ZIO are
-      // equivelant:
-      _ <-
-        printLine(
-          (
-            res1 == res2 && res2 == res3 &&
-              res3 == res4 && res4 == res5
+val testEqualities
+: ZIO[Console, IOException, Unit] =
+  for
+    res1: Int <- suc1
+    res2: Int <- suc2
+    res3: Int <- suc3
+    res4: Int <- suc4
+    res5: Int <- suc5
+    _ <-
+      printLine(s"""
+                   | res1: ${res1}
+                   | res2: ${res2}
+                   | res3: ${res3}
+                   | res4: ${res4}
+                   | res5: ${res5}
+                   | """.stripMargin)
+    _ <-
+      printLine(
+        (
+          res1 == res2 && res2 == res3 &&
+            res3 == res4 && res4 == res5
           ).toString
-        )
-      // Test if the differently aliased ZIO are
-      // considered equivelant:
-      _ <-
-        printLine(
-          (
-            suc1 == suc2 && suc2 == suc3 &&
-              suc3 == suc4 && suc4 == suc5
-          )
-        )
-      // Test if identically defined ZIO are
-      // considered equivelant:
-      _ <- printLine((suc4 == suc4d).toString)
-    yield ()
+      )
+    _ <-
+      printLine(
+        suc1 == suc2 && suc2 == suc3 &&
+          suc3 == suc4 && suc4 == suc5
+      )
+    _ <- printLine((suc4 == suc4duplicate).toString)
+  yield ()
 
-  // Until the ZIO are run, they cannot be
-  // considered equivelant to other ZIO. Becuase
-  // they represent a certain level of
-  // uncertainty, their exact value cannot be
-  // determined.
-
-  def run(args: List[String]) =
-    myAppLogic.exitCode
-end Equality
+object Equality extends ZIOAppDefault:
+  def run = testEqualities
