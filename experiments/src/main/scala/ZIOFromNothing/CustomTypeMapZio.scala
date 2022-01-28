3347da8978e6266e9fe77f15eaaebefca933e813
Bill Frasure <bill.frasure@gmail.com>
2022-01-27 21:57:49 -0700

    Work on composing our fake ZIO in a way that tuples the environment #time 40m #bruce #bill

--- experiments/src/main/scala/ZIOFromNothing/CustomTypeMapZio.scala
+++ experiments/src/main/scala/ZIOFromNothing/CustomTypeMapZio.scala
@@ -1,5 +1,7 @@
 package ZIOFromNothing
 
+import scalaBasics.forComprehension
+
 
 
 // trait TypeTag // TODO Or ClassTag?
@@ -18,8 +20,16 @@ case class CustomTypeMapZio[ENV, RESULT](
     def unsafeRun(env: ENV): RESULT =
         run(env)
 
+    // The tuple here is a step towards the full-featured TypeMap that ZIO uses
+    def flatMap[ENV2, RESULT2](
+        f: RESULT => CustomTypeMapZio[ENV2, RESULT2]
+    ): CustomTypeMapZio[(ENV, ENV2), RESULT2] =
+        CustomTypeMapZio(
+            (env, env2) => f(run(env)).run(env2)
+        )
+
 @main
-def demo =
+def demoSingleEnvironmentInstance =
     val customTypeMapZio: CustomTypeMapZio[Int, String] = 
         CustomTypeMapZio(
                 env =>
@@ -34,3 +44,25 @@ def demo =
             env => s"Message \n" * env
         )
     println(repeatMessage.unsafeRun(5))
+
+case class BigResult(message: String)
+@main
+def demoTupledEnvironment =
+    val squared: CustomTypeMapZio[Int, Unit] = 
+        CustomTypeMapZio(
+            env => println("Environment integer squared: " + env * env)
+        )
+
+    val repeatMessage: CustomTypeMapZio[String, BigResult] = 
+        CustomTypeMapZio(
+            message => BigResult(s"Environment message: $message")
+        )
+    
+    val composedRes: CustomTypeMapZio[(Int, String), BigResult] =
+        squared.flatMap(_ =>  repeatMessage)
+
+    val finalResult = composedRes.unsafeRun((5, "Hello"))
+    println(finalResult)
+
+
+import zio.ZIO
