292f706f374d8c6db7f1182790d6ef3b05ef7cf3
Bill Frasure <bill.frasure@gmail.com>
2022-02-04 22:35:23 -0700

    Attempt to wield NotGiven to prevent TypeMap duplicate entries. #bill #time 30m Turns out, I have no idea how it works.

--- experiments/src/main/scala/environment_exploration/ToyEnvironment.scala
+++ experiments/src/main/scala/environment_exploration/ToyEnvironment.scala
@@ -6,14 +6,17 @@ case class DBService(url: String)
 
 // Yada yada yada lets talk about the environment
 trait ToyEnvironmentT[+R]:
+
   def add[A: ClassTag](
       a: A
   ): ToyEnvironmentT[R & A]
+
   def get[A >: R: ClassTag]: A
 
 class ToyEnvironment[+R](
-    typeMap: Map[ClassTag[_], _]
+    typeMap: Map[ClassTag[_], Any]
 ) extends ToyEnvironmentT[R]:
+
   def add[A: ClassTag](
       a: A
   ): ToyEnvironment[R & A] =
@@ -41,5 +44,28 @@ def demoToyEnvironment =
   println(env3.get[DBService])
   println(env3.get[List[String]])
 
-// We get some amount of compile time safety here, but not much
-// println(env.get(classOf[List[DBService]]))
+  // We get some amount of compile time safety
+  // here, but not much
+  // println(env.get(classOf[List[DBService]]))
+
+  // Downside of the current approach is that it
+  // doesn't prevent duplicate types
+  env3.add("hi") // is accepted
+
+// Consider this runtime de-duping
+
+class ToyEnvironmentRuntimeDeduplication[+R](
+    typeMap: Map[ClassTag[_], Any]
+):
+
+  def add[A: ClassTag](
+      a: A
+  ): ToyEnvironment[R & A] =
+    if (typeMap.contains(classTag[A]))
+      throw new IllegalArgumentException(
+        s"Cannot add ${classTag[A]} to environment, it already exists"
+      )
+    else
+      ToyEnvironment(
+        typeMap + (classTag[A] -> a)
+      )
