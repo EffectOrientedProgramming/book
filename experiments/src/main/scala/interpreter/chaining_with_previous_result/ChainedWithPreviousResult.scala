afc48bc3e6fd8de331eda53d3db6d087558b9b60
Bill Frasure <bill.frasure@gmail.com>
2022-02-17 21:12:58 -0700

    Simplify ChainedWithPreviousResult types #bruce #bill #james #time 20m

--- experiments/src/main/scala/interpreter/chaining_with_previous_result/ChainedWithPreviousResult.scala
+++ experiments/src/main/scala/interpreter/chaining_with_previous_result/ChainedWithPreviousResult.scala
@@ -1,23 +1,20 @@
 package interpreter.chaining_with_previous_result
 
+import interpreter.level1_nochaining.Random
 import zio.{ZIO, ZIOAppDefault}
 
-trait Operation:
-  def actOn(input: String): String
+trait Operation
 
-case class Value(value: String) extends Operation:
-  def actOn(input: String): String = value
+case class Value(value: String) extends Operation
 
 case class StringManipulation(
                   action: String => String
                 ) extends Operation:
   def actOn(input: String): String = action(input)
 
-object Print extends Operation:
-  def actOn(input: String): String =
-    println(input)
-    input
+object Print extends Operation
 
+object RandomString extends Operation
 
 val program = Seq(
   Value("Hello There"),
@@ -25,12 +22,26 @@ val program = Seq(
   StringManipulation(_.toUpperCase().nn),
   Print,
   StringManipulation(_.take(5).nn),
-  Print
+  Print,
+  RandomString,
+  Print,
+  StringManipulation(_.toUpperCase().nn),
+  Print,
 )
 
 def interpret(program: Seq[Operation]): String =
   program.foldLeft("") { (acc, op) =>
-    op.actOn(acc)
+    op match {
+      case Print =>
+        println(acc)
+        acc
+      case RandomString =>
+        scala.util.Random.alphanumeric.take(10).mkString
+      case Value(value) =>
+        value
+      case StringManipulation(action) =>
+        action(acc)
+    }
   }
 
 @main
