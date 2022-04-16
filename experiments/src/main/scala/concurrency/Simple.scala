ebe2b593e6177102416f163ac2c85631b5dd3072
Scala Steward <me@scala-steward.org>
2022-04-16 20:15:59 +0200

    Reformat with scalafmt 3.5.1

--- experiments/src/main/scala/concurrency/Simple.scala
+++ experiments/src/main/scala/concurrency/Simple.scala
@@ -1,20 +1,35 @@
 package concurrency
 
-import zio.{durationInt, duration2DurationOps, Clock, Console, Duration, ZIO, ZIOAppDefault, Random}
+import zio.{
+  durationInt,
+  duration2DurationOps,
+  Clock,
+  Console,
+  Duration,
+  ZIO,
+  ZIOAppDefault,
+  Random
+}
 
-def sleepThenPrint(d: Duration): ZIO[Clock & Console, java.io.IOException, Duration] =
+def sleepThenPrint(d: Duration): ZIO[
+  Clock & Console,
+  java.io.IOException,
+  Duration
+] =
   for
     _ <- ZIO.sleep(d)
-    _ <- Console.printLine(s"${d.render} elapsed")
+    _ <-
+      Console.printLine(s"${d.render} elapsed")
   yield d
 
 object ForkDemo extends zio.ZIOAppDefault:
-  override def run = for
-    f1 <- sleepThenPrint(2.seconds).fork
-    f2 <- sleepThenPrint(1.seconds).fork
-    _  <- f1.join
-    _  <- f2.join
-  yield ()
+  override def run =
+    for
+      f1 <- sleepThenPrint(2.seconds).fork
+      f2 <- sleepThenPrint(1.seconds).fork
+      _  <- f1.join
+      _  <- f2.join
+    yield ()
 
 object ForEachDemo extends zio.ZIOAppDefault:
   override def run =
@@ -30,43 +45,56 @@ object ForEachParDemo extends zio.ZIOAppDefault:
 
 object RaceDemo extends zio.ZIOAppDefault:
   override def run =
-    ZIO.raceAll(sleepThenPrint(2.seconds),
+    ZIO.raceAll(
+      sleepThenPrint(2.seconds),
       Seq(sleepThenPrint(1.seconds))
     )
-    /*
-    // alternate syntax:
-    sleepThenPrint(2.seconds).race(Seq(sleepThenPrint(1.seconds))
-    */
+    /* // alternate syntax:
+     * sleepThenPrint(2.seconds).race(Seq(sleepThenPrint(1.seconds)) */
 
-object CollectAllParDemo extends zio.ZIOAppDefault:
+object CollectAllParDemo
+    extends zio.ZIOAppDefault:
   override def run =
     for
-     durations <- ZIO.collectAllPar(
-        Seq(
-          sleepThenPrint(2.seconds),
-          sleepThenPrint(1.seconds),
+      durations <-
+        ZIO.collectAllPar(
+          Seq(
+            sleepThenPrint(2.seconds),
+            sleepThenPrint(1.seconds)
+          )
         )
-      )
-     total = durations.fold(Duration.Zero)(_ + _).render
-     _ <- Console.printLine(total)
-    yield ()
-
-object CollectAllParMassiveDemo extends zio.ZIOAppDefault:
-  override def run =
-    for
-      durations <- ZIO.collectAllSuccessesPar(
-        Seq.fill(
-          1_000_000
-        )(1.seconds).map(duration =>
-          for {
-            randInt <- Random.nextIntBetween(0, 100)
-            _ <- ZIO.sleep(duration)
-            _ <- ZIO.when(randInt < 10)(
-              ZIO.fail("Number is too low")
-            )
-          } yield duration
-        )
-      )
-      total = durations.fold(Duration.Zero)(_ + _).render
+      total =
+        durations
+          .fold(Duration.Zero)(_ + _)
+          .render
       _ <- Console.printLine(total)
     yield ()
+end CollectAllParDemo
+
+object CollectAllParMassiveDemo
+    extends zio.ZIOAppDefault:
+  override def run =
+    for
+      durations <-
+        ZIO.collectAllSuccessesPar(
+          Seq
+            .fill(1_000_000)(1.seconds)
+            .map(duration =>
+              for
+                randInt <-
+                  Random.nextIntBetween(0, 100)
+                _ <- ZIO.sleep(duration)
+                _ <-
+                  ZIO.when(randInt < 10)(
+                    ZIO.fail("Number is too low")
+                  )
+              yield duration
+            )
+        )
+      total =
+        durations
+          .fold(Duration.Zero)(_ + _)
+          .render
+      _ <- Console.printLine(total)
+    yield ()
+end CollectAllParMassiveDemo
