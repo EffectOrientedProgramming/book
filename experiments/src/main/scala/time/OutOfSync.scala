de6020d2fe47af5a6dfee82fc7d624584bf06000
Bill Frasure <bill.frasure@gmail.com>
2022-06-15 14:37:54 -0600

    Show clock skew when making 2 calls in a row #bill #time 10m

--- experiments/src/main/scala/time/OutOfSync.scala
+++ experiments/src/main/scala/time/OutOfSync.scala
@@ -1,50 +1,74 @@
 package time
 
 import java.time.{Instant, Period}
-import zio.{UIO, ZIO, ZIOAppDefault}
+import zio.{IO, UIO, ZIO, ZIOAppDefault}
 
 object OutOfSync
 
+// TODO Consider deduping User throughout the book
 case class User(name: String)
 case class Post(content: String)
 case class Summary(numberOfPosts: Int)
 
 case class UserUI(
     user: User,
-                          summary: Summary,
-                          transactionDetails: Seq[Post]
-                        ):
-  val summaryCountVsDerivedCount =
-    s"Summary Count: ${summary.numberOfPosts}\nDerived Count: ${transactionDetails.size}"
+    summary: Summary,
+    transactionDetails: Seq[Post]
+)
 
 case class TransactionDetails(
-                               transactions: Seq[Post]
-                             )
+    transactions: Seq[Post]
+)
 
-val frop = User("Frop")
+val frop  = User("Frop")
 val zeb   = User("Zeb")
 val shtep = User("Shtep")
 val cheep = User("Cheep")
 
 object TimeIgnorant:
-  def summaryFor(participant: User): UIO[Summary] =
-    ZIO.succeed(
-      Summary(1)
-    )
+  private var summaryCalledTime
+      : Option[Instant] = None
+  def summaryFor(
+      participant: User
+  ): UIO[Summary] =
+    summaryCalledTime match
+      case Some(value) =>
+        ()
+      case None =>
+        summaryCalledTime =
+          Some(Instant.now().nn)
 
-  def transactionsFor(participant: User): UIO[Seq[Post]] =
-    ZIO.succeed(
-      Seq(
-        Post("Hello!"),
-        Post("Goodbye!"),
-      )
-    )
+    ZIO.succeed(Summary(1))
+
+  def postsBy(
+      participant: User
+  ): IO[String, Seq[Post]] =
+    val executionTimeStamp = Instant.now()
+    for
+      _ <-
+        ZIO
+          .getOrFailWith(
+            "Must call summary before posts"
+          )(summaryCalledTime)
+          .flatMap(timeStamp =>
+            ZIO.debug(
+              "Summary called: " + timeStamp
+            )
+          )
+      _ <-
+        ZIO.debug(
+          "Getting posts:  " + executionTimeStamp
+        )
+    yield Seq(Post("Hello!"), Post("Goodbye!"))
+  end postsBy
+end TimeIgnorant
 
 object DemoSyncIssues extends ZIOAppDefault:
   def run =
     for
       summary <- TimeIgnorant.summaryFor(shtep)
-      transactions <- TimeIgnorant.transactionsFor(shtep)
-      uiContents = UserUI(shtep, summary, transactions)
+      transactions <- TimeIgnorant.postsBy(shtep)
+      uiContents =
+        UserUI(shtep, summary, transactions)
       _ <- zio.Console.printLine(uiContents)
     yield ()
