f6e50487dc34183acd7b302b91794e5183e49927
Bill Frasure <bill.frasure@gmail.com>
2022-06-16 21:34:20 -0600

    Build Mutability+SensorReading demo #bill #bruce #time 40m

--- experiments/src/main/scala/mutability/Refs.scala
+++ experiments/src/main/scala/mutability/Refs.scala
@@ -41,6 +41,46 @@ object ReliableMutability
     yield ()
 
 
+object MutabilityWithComplexTypes
+  extends ZIOAppDefault:
+
+
+  case class Sensor():
+    def read: ZIO[Any, Nothing, SensorData] = zio.Random.nextIntBounded(10).map(SensorData(_))
+
+  case class SensorData(value: Int)
+
+  case class World(sensors: List[Sensor], currentData: Ref[List[SensorData]])
+
+  val arbitrarilyChangeWorldData =
+    for
+      currentData: Ref[List[SensorData]] <- Ref.make(List.fill(100)(0).map(SensorData(_)))
+      world: World = World(List.fill(100)(Sensor()), currentData)
+      _ <- world.currentData.get.debug("Current data: ")
+      _ <- world.currentData.update(oldData=>
+        oldData.zipWithIndex.map{case (_, idx) =>
+          if (idx < 10)
+            SensorData(5)
+          else
+            SensorData(0)
+        }
+      )
+      _ <- world.currentData.get.debug("Updated data: ")
+    yield ()
+
+  def run =
+
+    readFromSensors
+
+  val readFromSensors =
+    for
+      currentData: Ref[List[SensorData]] <- Ref.make(List.fill(100)(0).map(SensorData(_)))
+      world: World = World(List.fill(100)(Sensor()), currentData)
+      _ <- world.currentData.get.debug("Current data: ")
+      updatedSensorReadings <- ZIO.foreach(world.sensors)(_.read)
+      _ <- world.currentData.set(updatedSensorReadings)
+      _ <- world.currentData.get.debug("Updated data from sensors: ")
+    yield ()
 
 
 
