66ebaf11a0203be8ad46d775f1c01e244be58ec4
Bill Frasure <bill.frasure@gmail.com>
2022-07-20 16:00:09 -0600

    Start on Logging examples #bill 30m

---
+++ experiments/src/main/scala/logging/Logging.scala
@@ -0,0 +1,48 @@
+package logging
+
+import zio.logging.*
+import zio.*
+import zio.logging.LogFormat.{label, line, quoted, text}
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object Logging extends ZIOAppDefault {
+
+  lazy val minimal: LogFormat =
+    label("message", quoted(line)).highlight
+
+  lazy val locationLogger: LogFormat =
+     location(new WackyGps) |-| label("message", quoted(line)).highlight
+
+
+
+  lazy val coloredLogger = {
+    Runtime.removeDefaultLoggers >>>
+      console(
+//        LogFormat.colored
+          locationLogger
+      )
+  }
+
+  def run =
+    ZIO.log("Hi").provide(coloredLogger)
+
+
+//  val timestamp: LogFormat = timestamp(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+
+  def location(gps: Gps): LogFormat =
+    text {
+      gps.currentLocation().toString
+    }
+}
+
+enum Continent:
+  case NorthAmerica, SouthAmerica, Europe, Asia, Antarctica, Australia, Africa
+
+trait Gps:
+  def currentLocation(): Continent
+
+class WackyGps extends Gps:
+  def currentLocation(): Continent =
+    Continent.values.apply(scala.util.Random.between(0, Continent.values.length))
