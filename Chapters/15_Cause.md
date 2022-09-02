203c8fd04b4661c4e3c5a5c27719952edef9687f
Bill Frasure <bill.frasure@gmail.com>
2022-09-01 21:48:37 -0600

    More cause exploration #bruce #bill #james #time 24m

--- Chapters/15_Cause.md
+++ Chapters/15_Cause.md
@@ -3,4 +3,12 @@
 Consider an Evolutionary example, where a `Cause` allows us to track 
 MutationExceptions throughout a length process
 
+Cause will track all errors that occur in an application, regardless of concurrency and parallelism
+
 Cause allows you to aggregate multiple errors of the same type
+
+&&/Both represents parallel failures
+++/Then represents sequential failures
+
+Cause.die will show you the line that failed, because it requires a throwable
+Cause.fail will not, because it can be any arbitrary type
--- experiments/src/main/scala/cause/CauseBasics.scala
+++ experiments/src/main/scala/cause/CauseBasics.scala
@@ -2,9 +2,20 @@ package cause
 
 import zio._
 
-object CauseBasics extends ZIOAppDefault {
-  def run =
+object CauseBasics extends App {
 //    ZIO.fail(Cause.fail("Blah"))
-    ZIO.fail(Cause.die(Exception("Blah")) ++ Cause.die(Exception("Dee")))
+    println(
+      (Cause.die(Exception("1")) ++
+        (Cause.fail(Exception("2a")) && Cause.fail(Exception("2b"))) ++
+          Cause.stackless(Cause.fail(Exception("3"))
+          )
+        ).prettyPrint)
 
 }
+
+object CauseZIO extends ZIOAppDefault:
+
+  val x: ZIO[Any, Nothing, Nothing] =
+    ZIO.die(Exception("Blah"))
+  def run =
+    ZIO.die(Exception("Blah"))
