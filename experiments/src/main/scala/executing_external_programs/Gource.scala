bec3d71d72de841d24a0c1142384074b47ade7e1
Bill Frasure <bill.frasure@gmail.com>
2022-09-12 21:31:58 -0600

    Expand Gource demo #bill #time 20m

--- experiments/src/main/scala/executing_external_programs/Gource.scala
+++ experiments/src/main/scala/executing_external_programs/Gource.scala
@@ -11,13 +11,34 @@ import zio.process.{Command, ProcessInput, ProcessOutput}
       - More recent activity
     - Cycle between different repositories
 */
-object Gource extends ZIOAppDefault {
+object GourceDemo extends ZIOAppDefault {
 
-
-  def run =
-    Command("gource",
+  def gource(repoDir: String) = Command("gource",
 //      "--follow-user", "bfrasure", // Highlights user, but still shows others
-      "--user-show-filter", "bfrasure", // Only shows user
-      "/Users/bfrasure/Repositories/book"
-    ).run
+      "--user-show-filter", "bfrasure|Bill Frasure", // Only shows user
+      repoDir
+    )
+
+  val projects =
+    List(
+      "/Users/bfrasure/Repositories/book",
+      "/Users/bfrasure/Repositories/TestFrameworkComparison",
+    )
+
+  def showActivityForAWhile(repoDir: String) =
+    for {
+      run1 <- gource(repoDir).run
+      _ <- ZIO.sleep(5.seconds)
+      _ <- run1.killForcibly
+    } yield ()
+
+  def randomProjectActivity =
+    for {
+      idx <- Random.nextIntBounded(projects.length)
+      _ <- showActivityForAWhile(projects(idx))
+    } yield ()
+  def run =
+    for {
+      _ <- randomProjectActivity.repeatN(2)
+    } yield ()
 }
