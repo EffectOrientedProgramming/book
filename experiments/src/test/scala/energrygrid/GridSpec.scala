92f99468795fc0d14374bcf7648f64253f8735fa
Bill Frasure <bill.frasure@gmail.com>
2022-10-08 22:26:52 -0600

    More Electric Grid #bill #time 30m

--- experiments/src/test/scala/energrygrid/GridSpec.scala
+++ experiments/src/test/scala/energrygrid/GridSpec.scala
@@ -2,20 +2,37 @@ package energrygrid
 
 import zio._
 import zio.test._
+import energrygrid.GridErrors._
 
-trait EnergySource
-trait EnergySink
+/*
+  TODO Simulate a home-level grid managing power needs and production
+    Why?
+      - This is code that interacts with TheWorld in numerous ways
+      - It's something I've wanted a more visceral understanding of
+*/
+trait EnergyParticipant
+
+enum EnergyProducer extends EnergyParticipant:
+  case SolarPanels, Generator
+  def sendPowerTo(consumer: EnergyConsumer | EnergyBidirectional): ZIO[Any, Overheat, Unit] = ???
+
+enum EnergyConsumer extends EnergyParticipant:
+  case Dishwasher, WiFi, Refrigerator, Lights
+  def drawPowerFrom(producer: EnergyProducer | EnergyBidirectional): ZIO[Any, InsufficientPower, Unit] = ???
+
+enum EnergyBidirectional extends EnergyParticipant:
+  case HomeBattery, ElectricVehicle, ExternalGrid
+  def sendPowerTo(consumer: EnergyConsumer | EnergyBidirectional): ZIO[Any, Overheat, Unit] = ???
+  def drawPowerFrom(producer: EnergyProducer | EnergyBidirectional): ZIO[Any, InsufficientPower, Unit] = ???
 
 case class Grid(
-  sources: Set[EnergySource],
-  sinks: Set[EnergySink]
+  participants: Set[EnergyParticipant]
 )
 
-case class Appliance() extends EnergySink:
-  val run: ZIO[Grid, errors.Unpowered, Unit] = ???
+trait MunicipalGrid
 
 case class User():
-  val live: ZIO[Clock, errors.UnsatisfiedNeeds, Unit] = ???
+  val live: ZIO[Clock, UnsatisfiedNeeds, Unit] = ???
 
 case class Home(
                  family: User,
@@ -26,9 +43,13 @@ case class Home(
       _ <- family.live
     } yield ()
 
-package errors:
+sealed trait GridErrors
+
+object GridErrors:
   trait Unpowered
   trait UnsatisfiedNeeds
+  trait InsufficientPower
+  trait Overheat
 
 
 object GridSpec extends ZIOSpecDefault {
