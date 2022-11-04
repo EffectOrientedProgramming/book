364d4ccba12e282953bec0bbdd65365c79be3aea
James Ward <james@jamesward.com>
2022-11-03 20:35:12 -0600

    timed tap tap #bruce #bill #james #time 50m

---
+++ experiments/src/main/scala/time/TimedTapTap.scala
@@ -0,0 +1,23 @@
+package time
+
+import zio.*
+import zio.Console.*
+
+val longRunning = ZIO.sleep(5.seconds) *> printLine("done")
+
+val runningNotifier = (ZIO.sleep(1.seconds) *> printLine("Still running")).onInterrupt {
+  printLine("finalized").orDie
+}
+
+object TimedTapTapJames extends ZIOAppDefault:
+
+  def run =
+    for
+      lr <- longRunning.fork
+      _ <- runningNotifier.fork
+      _ <- lr.join
+    yield ()
+
+object TimedTapTapBill extends ZIOAppDefault:
+  def run =
+    longRunning.race(runningNotifier *> ZIO.never)
