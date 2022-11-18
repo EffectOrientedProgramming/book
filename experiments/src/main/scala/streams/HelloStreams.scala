7591d7de61065fc90bab48d2e1bddfc22d038a62
Bill Frasure <bill.frasure@gmail.com>
2022-11-18 14:51:46 -0600

    Tinker with Streams  #bill #time 1h

---
+++ experiments/src/main/scala/streams/HelloStreams.scala
@@ -0,0 +1,16 @@
+package streams
+
+import zio.*
+import zio.stream.*
+
+object HelloStreams extends ZIOAppDefault:
+  def run =
+    for
+      _ <- ZIO.debug("Stream stuff!")
+      greetingStream = ZStream.repeatWithSchedule("Hi", Schedule.spaced(1.seconds))
+      insultStream = ZStream.repeatWithSchedule("Dummy", Schedule.spaced(2.seconds))
+      combinedStream = ZStream.mergeAllUnbounded()(greetingStream, insultStream)
+      aFewElements = combinedStream.take(6)
+      res <- aFewElements.run(ZSink.collectAll)
+      _ <- ZIO.debug("Res: " + res)
+    yield ()
