33b5e0e662d3998b8d57bd66267760eb06684a8a
Bill Frasure <bill.frasure@gmail.com>
2022-12-11 21:47:36 -0700

    Throw out all but the simplest tweet csv entries. I don't want to get sidetracked by parsing riddles  #bill #time 15m

--- experiments/src/main/scala/streams/TwitterCustomerSupport.scala
+++ experiments/src/main/scala/streams/TwitterCustomerSupport.scala
@@ -9,18 +9,39 @@ object TwitterCustomerSupport extends ZIOAppDefault:
   def run =
     for
       _ <- ZIO.debug("Hi")
-      tweetStream = ZStream.fromFileName("datasets/sample.csv")
+      tweetStream = ZStream.fromFileName("../datasets/sample.csv").drop(1)
       currentLine <- Ref.make[Chunk[Byte]](Chunk.empty)
-      _ <- tweetStream.mapZIO(
+      linesMaybe = tweetStream.mapZIO(
         byte =>
           if (byte ==  0xA)
             for
               lineContents <- currentLine.getAndSet(Chunk.empty)
-              _ <- ZIO.debug("Line: " + (new String(lineContents.appended(byte).toArray)))
-            yield ()
+              line = new String(lineContents.appended(byte).toArray)
+//              _ <- ZIO.debug("Line: " + (line))
+            yield Some(line)
           else
-            currentLine.update(_.appended(byte))
+            currentLine.update(_.appended(byte)) *> ZIO.succeed(None)
+
 //            ZIO.debug("TODO Collect bytes here!")
-      ).runDrain
+      )
+      lines = linesMaybe.map(o => ZStream.fromIterable(o)).flatten
+      _ <- lines.foreach(l => ZIO.debug("Line: " + Tweet(l)))
 //      _ <- tweetStream.foreach( byte => ZIO.when(byte == 0xA)(ZIO.debug("New Line")))
     yield ()
+
+  case class ParsingError(msg: String)
+  case class Tweet(tweet_id: String, author_id: String, inbound: Boolean, created_at: String, text: String, response_tweet_id: Option[String], in_response_to_tweet_id: Option[String])
+  object Tweet:
+    def apply(csvLine: String): Either[ParsingError, Tweet] =
+      val pieces = csvLine.split(",")
+      Either.cond(
+        pieces.length == 7,
+        pieces match
+          case Array(tweet_id, author_id, inbound, created_at, text, response_tweet_id, in_response_to_tweet_id) =>
+            Tweet(
+                tweet_id, author_id, inbound == "True", created_at, text, Some(response_tweet_id), Some(in_response_to_tweet_id)
+            )
+          case _ => ???
+        ,
+       ParsingError("Bad value: " + pieces)
+    )
