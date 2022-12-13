b8ea36998311d45dc88e25a167f0ffdb532531f7
Bill Frasure <bill.frasure@gmail.com>
2022-12-12 17:40:06 -0700

    Significantly improve Twitter parsing performance by eliminating some boxing and flatmapped Streams  #bill #time 1h

--- experiments/src/main/scala/streams/TwitterCustomerSupport.scala
+++ experiments/src/main/scala/streams/TwitterCustomerSupport.scala
@@ -1,48 +1,44 @@
 package streams
 
-import zio._
-import zio.stream._
+import zio.*
+import zio.stream.*
+
+import java.time.Instant
 
 // This currently runs against the dataset available here:
 // https://www.kaggle.com/datasets/thoughtvector/customer-support-on-twitter?resource=download
 object TwitterCustomerSupport
     extends ZIOAppDefault:
-  val fileName = "../datasets/sample.csv"
-//    "../datasets/twcs/twcs.csv"
+  val fileName =
+//    "../datasets/sample.csv"
+    "../datasets/twcs/twcs.csv"
+
   def run =
     for
-      _ <- ZIO.debug("Hi")
+      startTime <- Clock.instant
       tweetStream =
         ZStream.fromFileName(fileName).drop(1)
+      lineNumber <- Ref.make(0)
       currentLine <-
         Ref.make[Chunk[Byte]](Chunk.empty)
       linesMaybe =
         tweetStream.mapZIO(byte =>
-          if (byte == 0xa)
-            for
-              lineContents <-
-                currentLine
-                  .getAndSet(Chunk.empty)
-              line =
-                new String(
-                  lineContents
-                    .appended(byte)
-                    .toArray
-                )
-//              _ <- ZIO.debug("Line: " + (line))
-            yield Some(line)
-          else
-            currentLine
-              .update(_.appended(byte)) *>
-              ZIO.succeed(None)
+              gatherLines(byte, currentLine, lineNumber, startTime)
         )
       lines =
         linesMaybe
-          .flatMap(o => ZStream.fromIterable(o))
+          .filter(_.isDefined)
+          .map(_.get)
+//        linesMaybe
+//          .flatMap(o => ZStream.fromIterable(o))
       tweets =
         lines.flatMap(l =>
           ZStream.fromIterable(Tweet(l).toOption)
         )
+        lines.map(l =>
+          Tweet(l)
+        ).filter(_.isRight)
+          .map(_.getOrElse(???))
       activeCompanies <-
         Ref.make[Map[String, Int]](Map.empty)
       companyActivity =
@@ -56,8 +52,45 @@ object TwitterCustomerSupport
               )
           yield companies.maxBy(_._2)
         )
-      _ <- companyActivity.debug.runDrain
+      _ <-
+        companyActivity
+          .tap( (name, count) =>
+            ZIO.when(count % 100 == 0)(
+              ZIO.debug("Active company: " + name + "  " + count + " interactions.")
+            )
+          )
+//        .debug
+//        lines
+//        linesMaybe
+        .runDrain
+        .timeout(30.seconds)
     yield ()
+
+  def gatherLines(byte: Byte, currentLine: Ref[Chunk[Byte]], lineNumber: Ref[Int], startTime: Instant) =
+    if (byte == 0xa)
+      for
+        lineContents <-
+          currentLine
+            .getAndSet(Chunk.empty)
+        currentLineNumber <- lineNumber.updateAndGet(_ + 1)
+        _ <- ZIO.when(currentLineNumber % 10000 == 0)(
+          for
+            currentTime <- Clock.instant
+            runtime = java.time.Duration.between(startTime, currentTime)
+            _ <- ZIO.debug("Line number: " + currentLineNumber + ". Time per 10,000 rows: " + runtime)
+          yield ()
+        )
+        line =
+          new String(
+            lineContents
+              .appended(byte)
+              .toArray
+          )
+      yield Some(line)
+    else
+      currentLine
+        .update(_.appended(byte)) *>
+        ZIO.succeed(None)
 
   private def incrementCompanyActivity(
       value1: Map[String, Int],
