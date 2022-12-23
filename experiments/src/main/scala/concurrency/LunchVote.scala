ff79df8b19322f9601e970bc09be365eacc5cf50
Bill Frasure <bill.frasure@gmail.com>
2022-12-23 11:24:07 -0700

    Simplify voting example to use race instead of manual fiber manipulation  #bill #time 15m

--- experiments/src/main/scala/concurrency/LunchVote.scala
+++ experiments/src/main/scala/concurrency/LunchVote.scala
@@ -7,35 +7,80 @@ import zio.concurrent.*
 object LunchVote extends ZIOAppDefault:
 
   enum Vote:
-    case Yay, Nay
-
-
-  def getVoteFrom(person: String, results: ConcurrentMap[Vote, Int], votingComplete: Promise[Nothing, Vote], voterCount: Int): ZIO[Any, Nothing, Vote] =
-    (for
-      sleepAmount <- Random.nextIntBetween(1, 5)
-      _ <- ZIO.sleep(sleepAmount.seconds)
-      answer <- Random.nextBoolean.map(b => if(b) Vote.Yay else Vote.Nay).debug(s"$person vote")
-      currentTally <- results.computeIfPresent(answer, (_, previous) => previous + 1).someOrFail(IllegalStateException("Vote not found")).orDie
-      _ <- ZIO.when(currentTally > voterCount / 2)(votingComplete.succeed(answer))
-//      _ <- votingComplete.isDone.debug(s"Voting complete when $person voted")
-    yield answer).onInterrupt(ZIO.debug(s"interrupted $person vote"))
+    case Yay,
+      Nay
 
   def run =
-    val voters = List(
-      "Alice",
-      "Bob",
-      "Charlie",
-      "Dave",
-      "Eve",
-    )
+    val voters =
+      List(
+        "Alice",
+        "Bob",
+        "Charlie",
+        "Dave",
+        "Eve"
+      )
     for
-      resultMap <- ConcurrentMap.make[Vote, Int](Vote.Yay -> 0, Vote.Nay -> 0)
-      votingComplete <- Promise.make[Nothing, Vote]
-      ongoingVotes <- ZIO.forkAll(
-        voters.map(voter => getVoteFrom(voter, resultMap, votingComplete, voters.size)
-        ))
-      _ <- votingComplete.await.debug("Result")
-//      _ <- votingComplete.isDone.debug("Done")
-      _ <- ongoingVotes.interrupt
-//      _ <- results.
+      resultMap <-
+        ConcurrentMap.make[Vote, Int](
+          Vote.Yay -> 0,
+          Vote.Nay -> 0
+        )
+      voteProcesses = voters.map(voter =>
+            getVoteFrom(
+              voter,
+              resultMap,
+              voters.size
+            ).onInterrupt(
+              ZIO.debug(s"interrupted $voter")
+            )
+          )
+      result <-
+        ZIO.raceAll(
+          voteProcesses.head,
+          voteProcesses.tail,
+        )
+      _ <- ZIO.debug("Result: " + result)
     yield ()
+    end for
+  end run
+
+  case object NotConclusive
+
+  def getVoteFrom(
+                   person: String,
+                   results: ConcurrentMap[Vote, Int],
+                   voterCount: Int
+                 ): ZIO[Any, NotConclusive.type, Vote] =
+    for
+      sleepAmount <-
+        Random.nextIntBetween(1, 5)
+      _ <- ZIO.sleep(sleepAmount.seconds)
+      answer <-
+        Random
+          .nextBoolean
+          .map(b =>
+            if (b)
+              Vote.Yay
+            else
+              Vote.Nay
+          )
+          .debug(s"$person vote")
+      currentTally <-
+        results
+          .computeIfPresent(
+            answer,
+            (key, previous) => previous + 1
+          )
+          .someOrFail(
+            IllegalStateException(
+              "Vote not found"
+            )
+          )
+          .orDie
+      _ <-
+        ZIO.when(
+          currentTally <= voterCount / 2
+        )(ZIO.fail(NotConclusive))
+    yield answer
+
+end LunchVote
