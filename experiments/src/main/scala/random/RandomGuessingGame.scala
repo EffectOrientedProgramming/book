f4ca74cfef87e7426b3266b5ab63f077f4e5a684
Bill Frasure <bill.frasure@gmail.com>
2022-12-28 10:31:25 -0700

    Add many variations of GuessingGame logic #bill #time 20m

--- experiments/src/main/scala/random/RandomGuessingGame.scala
+++ experiments/src/main/scala/random/RandomGuessingGame.scala
@@ -14,6 +14,58 @@ def checkAnswer( answer: Int, guess: String ): String =
   if answer == guess.toInt then "You got it!"
   else s"BZZ Wrong!! Answer was $answer"
 
+def checkAnswerTry( answer: Int, guess: String ): String =
+  try
+    if answer == guess.toInt then "You got it!"
+    else s"BZZ Wrong!! Answer was $answer"
+  catch
+    case _ => "User did not provide an integer. Invalid input: " + guess
+
+def checkAnswerZ1( answer: Int, guess: String ): ZIO[Any, Nothing, String] =
+  ZIO.attempt(guess.toInt).map( i =>
+    if answer == i then "You got it!"
+    else s"BZZ Wrong!! Answer was $answer"
+  ).catchAll(_ => ZIO.succeed("Invalid input: " + guess))
+
+def checkAnswerZ2( answer: Int, guess: String ): ZIO[Any, Nothing, String] =
+  ZIO.attempt(guess.toInt).map( i =>
+    if answer == i then "You got it!"
+    else s"BZZ Wrong!! Answer was $answer"
+  ).orElseSucceed("Invalid input: " + guess)
+
+def checkAnswerZ3( answer: Int, guess: String ): ZIO[Any, Nothing, String] =
+  ZIO.attempt(guess.toInt)
+    .orElseFail("Invalid input:  " + guess)
+    .map( i =>
+    if answer == i then "You got it!"
+    else s"BZZ Wrong!! Answer was $answer"
+  ).merge
+
+def checkAnswerZ4( answer: Int, guess: String ): ZIO[Any, Nothing, String] =
+  ZIO.attempt(guess.toInt)
+    .mapBoth(
+      _ => "Invalid input:  " + guess,
+      i =>
+        if answer == i then "You got it!"
+        else s"BZZ Wrong!! Answer was $answer"
+    ).merge
+
+// After writing so many variations of the function above,
+// I suspect that what we _really_ need to do is break up the logic.
+// Both ZIO and vanilla Scala code should get simpler
+
+def parse(guess: String) =
+  ZIO.attempt(guess.toInt).orElseFail("Invalid input:  " + guess)
+
+def checkAnswerZSplit( answer: Int, guess: String ): ZIO[Any, Nothing, String] =
+  parse(guess)
+    .map(
+      i =>
+        if answer == i then "You got it!"
+        else s"BZZ Wrong!! Answer was $answer"
+    ).merge
+
+
 val sideEffectingGuessingGame =
   for
     _ <- Console.print(prompt)
@@ -23,24 +75,32 @@ val sideEffectingGuessingGame =
   yield prompt + guess + "\n" + response
 
 object runSideEffectingGuessingGame extends ZIOAppDefault:
-
   def run =
     sideEffectingGuessingGame
+      .withConsole(FakeConsole.single("3"))
       .debug("Side effecting results")
-      .provideLayer(
-      ZLayer.succeed(FakeConsole.single("3")) )
 
 val effectfulGuessingGame =
   for
     _ <- Console.print(prompt)
     answer <- RandomBoundedInt.nextIntBetween(low, high)
     guess <- Console.readLine
-    response = checkAnswer(answer, guess)
+    response <- checkAnswerZSplit(answer, guess)
   yield prompt + guess + "\n" + response
 
 object RunEffectfulGuessingGame extends ZIOAppDefault:
   def run =
-    effectfulGuessingGame.provideLayer(
-      ZLayer.succeed(
-        FakeConsole.single("3")) ++ RandomBoundedInt.live
+    effectfulGuessingGame
+      .withConsole(FakeConsole.single("3"))
+      .provideLayer(
+        RandomBoundedInt.live
+      )
+
+object RunEffectfulGuessingGameTestable extends ZIOAppDefault:
+  def run =
+    effectfulGuessingGame
+      .debug("Result")
+      .withConsole(FakeConsole.single("3"))
+      .provideLayer(
+        RandomBoundedIntFake(Seq(3))
     )
