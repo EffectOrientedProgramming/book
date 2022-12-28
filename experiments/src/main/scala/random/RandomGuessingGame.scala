11a5a15eaa0f4f6d2c378382a9070f678cbdb195
Bill Frasure <bill.frasure@gmail.com>
2022-12-28 09:56:52 -0700

    Restore random guessing game example #bill #time 15m

--- experiments/src/main/scala/random/RandomGuessingGame.scala
+++ experiments/src/main/scala/random/RandomGuessingGame.scala
@@ -1,48 +1,46 @@
 package random
 
-import zio.{Console, UIO, Unsafe, ZIO, ZLayer}
-//import console.FakeConsole
-import zio.Runtime.default.unsafe
+import console.FakeConsole
+import zio._
 
 val low = 1
-/* val high = 10
- *
- * val prompt =
- * s"Pick a number between $low and $high: "
- *
- * // TODO Determine how to handle .toInt failure
- * // possibility def checkAnswer( answer: Int,
- * guess: String ): String =
- * if answer == guess.toInt then "You got it!"
- * else s"BZZ Wrong!! Answer was $answer"
- *
- * val sideEffectingGuessingGame =
- * for _ <- Console.print(prompt) answer =
- * scala.util.Random.between(low, high) guess <-
- * Console.readLine response =
- * checkAnswer(answer, guess) yield prompt +
- * guess + "\n" + response
- *
- * @main def runSideEffectingGuessingGame =
- * Unsafe.unsafe { (u: Unsafe) => given Unsafe =
- * u unsafe .run(
- * sideEffectingGuessingGame.provideLayer(
- * ZLayer.succeed(FakeConsole.single("3")) ) )
- * .getOrThrowFiberFailure() }
- *
- * import zio.Console.printLine
- *
- * val effectfulGuessingGame =
- * for _ <- Console.print(prompt) answer <-
- * RandomBoundedInt.nextIntBetween(low, high)
- * guess <- Console.readLine response =
- * checkAnswer(answer, guess) yield prompt +
- * guess + "\n" + response
- *
- * @main def runEffectfulGuessingGame =
- * Unsafe.unsafe { (u: Unsafe) => given Unsafe =
- * u unsafe .run(
- * effectfulGuessingGame.provideLayer( ZLayer
- * .succeed(FakeConsole.single("3")) ++
- * RandomBoundedInt.live ) )
- * .getOrThrowFiberFailure() } */
+val high = 10
+
+val prompt =
+  s"Pick a number between $low and $high: "
+
+// TODO Determine how to handle .toInt failure possibility
+def checkAnswer( answer: Int, guess: String ): String =
+  if answer == guess.toInt then "You got it!"
+  else s"BZZ Wrong!! Answer was $answer"
+
+val sideEffectingGuessingGame =
+  for
+    _ <- Console.print(prompt)
+    answer = scala.util.Random.between(low, high)
+    guess <- Console.readLine
+    response = checkAnswer(answer, guess)
+  yield prompt + guess + "\n" + response
+
+object runSideEffectingGuessingGame extends ZIOAppDefault:
+
+  def run =
+    sideEffectingGuessingGame
+      .debug("Side effecting results")
+      .provideLayer(
+      ZLayer.succeed(FakeConsole.single("3")) )
+
+val effectfulGuessingGame =
+  for
+    _ <- Console.print(prompt)
+    answer <- RandomBoundedInt.nextIntBetween(low, high)
+    guess <- Console.readLine
+    response = checkAnswer(answer, guess)
+  yield prompt + guess + "\n" + response
+
+object RunEffectfulGuessingGame extends ZIOAppDefault:
+  def run =
+    effectfulGuessingGame.provideLayer(
+      ZLayer.succeed(
+        FakeConsole.single("3")) ++ RandomBoundedInt.live
+    )
