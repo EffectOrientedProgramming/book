cb685e6e5d9e7917cc2c8274724487b018377623
Bill Frasure <bill.frasure@gmail.com>
2023-01-07 20:16:23 -0700

    Get ThunderingHerds compiling. Definitely not working though!  #bill #time 30m

--- experiments/src/main/scala/concurrency/ThunderingHerds.scala
+++ experiments/src/main/scala/concurrency/ThunderingHerds.scala
@@ -2,37 +2,60 @@ package concurrency
 
 import zio._
 
+trait FileSystem
+trait FileNotFound
+trait RetrievalFailure
+
 trait FileService:
-    def retrieveContents(name: String): ZIO[Any, Nothing, List[String]]
+    def retrieveContents(name: String): ZIO[FileSystem, RetrievalFailure | FileNotFound, List[String]]
 
 object FileService:
-    trait FileSystem
-    trait FileNotFound
-    def readFile(name: String): ZIO[FileSystem, FileNotFound, List[String]] = ???
+    def readFile(name: String): ZIO[FileSystem, RetrievalFailure | FileNotFound, List[String]] =
+      ZIO.succeed(
+        List("line1", "line2")
+      ).debug("Read from FileSystem")
 
-    trait RetrievalFailure
+    val live =
+      ZLayer.fromZIO(
+        for
+          accessCount <- Ref.make[Int](0)
+          cache <- Ref.make[Map[String, List[String]]](Map.empty)
+          activeRefreshes <- Ref.make[Map[String, Promise[RetrievalFailure, List[String]]]](Map.empty)
+        yield Live(accessCount, cache, activeRefreshes)
+      )
+
     case class Live(
         accessCount: Ref[Int],
         cache: Ref[Map[String, List[String]]],
         activeRefresh: Ref[Map[String, Promise[RetrievalFailure, List[String]]]]
         ) extends FileService:
-            def retrieveContents(name: String): ZIO[Any, Nothing, List[String]] =
+            def retrieveContents(name: String): ZIO[FileSystem, RetrievalFailure | FileNotFound, List[String]] =
                 for
-                  initialValue <- cache.get(name)
+                  currentCache <- cache.get
+                  initialValue = currentCache.get(name)
                   activeValue <-
-                    ZIO.when(initialValue.isEmpty)
-                      activeRefresh(name) match
-                        case Some(pendingValue) =>
-                           pendingValue.await
-                        case None =>
-                           for
-                             refreshAttempt <- Promise.make[RetrievalFailure, List[String]]
-                             _ <- activeRefresh.update(_.set(name, refreshAttempt))
-                             contentsFromDisk <- readFile(name)
-                             _ <- refreshAttempt.succeed(contentsFromDisk)
-                           yield contentsFromDisk
+                    initialValue match
+                      case Some(initValue) =>
+                        ZIO.succeed(initValue)
+                      case None =>
+                          retrieveOrWaitForContents(name)
                 yield activeValue
 
+            def retrieveOrWaitForContents(name: String) =
+              for
+                currentRefreshes <- activeRefresh.get
+                result <-  currentRefreshes.get(name) match
+                    case Some(pendingValue) =>
+                       pendingValue.await
+                    case None =>
+                       for
+                         refreshAttempt <- Promise.make[RetrievalFailure, List[String]]
+                         _ <- activeRefresh.update(x => x.+((name, refreshAttempt)))
+                         contentsFromDisk <- readFile(name)
+                         _ <- refreshAttempt.succeed(contentsFromDisk)
+                         _ <- activeRefresh.update(x => x.-(name))
+                       yield contentsFromDisk
+              yield result
 
 
 
@@ -43,8 +66,11 @@ val users = List(
 )
 
 val herdBehavior =
-    ZIO.foreachPar(users)(retrieveContents("awesomeMemes"))
+  for
+    fileService <- ZIO.service[FileService]
+    _ <- ZIO.foreachPar(users)(user => fileService.retrieveContents("awesomeMemes"))
+  yield ()
 
 object ThunderingHerds extends ZIOAppDefault:
   def run =
-    herdBehavior
+    herdBehavior.provide(FileService.live, ZLayer.succeed(new FileSystem {}))
