9eaa52972920dfd278264b20f42af7efe873065b
Bill Frasure <bill.frasure@gmail.com>
2023-01-07 21:52:14 -0700

    Rough ThunderingHerds solution :)  #bill #time 45m

--- experiments/src/main/scala/concurrency/ThunderingHerds.scala
+++ experiments/src/main/scala/concurrency/ThunderingHerds.scala
@@ -1,6 +1,6 @@
 package concurrency
 
-import zio._
+import zio.*
 
 trait FileSystem
 trait FileNotFound
@@ -13,21 +13,21 @@ object FileService:
     def readFile(name: String): ZIO[FileSystem, RetrievalFailure | FileNotFound, List[String]] =
       ZIO.succeed(
         List("line1", "line2")
-      ).debug("Read from FileSystem")
+      ).debug("Read from FileSystem").delay(2.seconds)
 
     val live =
       ZLayer.fromZIO(
         for
           accessCount <- Ref.make[Int](0)
           cache <- Ref.make[Map[String, List[String]]](Map.empty)
-          activeRefreshes <- Ref.make[Map[String, Promise[RetrievalFailure, List[String]]]](Map.empty)
+          activeRefreshes <- Ref.Synchronized.make[Map[String, Promise[RetrievalFailure, List[String]]]](Map.empty)
         yield Live(accessCount, cache, activeRefreshes)
       )
 
     case class Live(
         accessCount: Ref[Int],
         cache: Ref[Map[String, List[String]]],
-        activeRefresh: Ref[Map[String, Promise[RetrievalFailure, List[String]]]]
+        activeRefresh: Ref.Synchronized[Map[String, Promise[RetrievalFailure, List[String]]]]
         ) extends FileService:
             def retrieveContents(name: String): ZIO[FileSystem, RetrievalFailure | FileNotFound, List[String]] =
                 for
@@ -41,23 +41,34 @@ object FileService:
                           retrieveOrWaitForContents(name)
                 yield activeValue
 
+            enum RefreshState:
+              case NewlyActive, AlreadyActive
+
             def retrieveOrWaitForContents(name: String) =
               for
-                currentRefreshes <- activeRefresh.get
-                result <-  currentRefreshes.get(name) match
-                    case Some(pendingValue) =>
-                       pendingValue.await
+                state <- Promise.make[Nothing, RefreshState]
+                promise <- activeRefresh.updateAndGetZIO { activeRefreshes =>
+                  activeRefreshes.get(name) match
+                    case Some(promise) =>
+                        state.succeed(RefreshState.AlreadyActive) *>
+                          ZIO.succeed(activeRefreshes)
                     case None =>
-                       for
-                         refreshAttempt <- Promise.make[RetrievalFailure, List[String]]
-                         _ <- activeRefresh.update(x => x.+((name, refreshAttempt)))
-                         contentsFromDisk <- readFile(name)
-                         _ <- refreshAttempt.succeed(contentsFromDisk)
-                         _ <- activeRefresh.update(x => x.-(name))
-                       yield contentsFromDisk
-              yield result
+                      for
+                        promise <- Promise.make[RetrievalFailure, List[String]]
+                        _ <- state.succeed(RefreshState.NewlyActive)
+                      yield activeRefreshes + (name -> promise)
+                }.map(_(name)) // TODO Unsafe
+                finalStatus <- state.await
+                finalContents <- finalStatus match
+                        case RefreshState.NewlyActive =>
+                          for
+                            contents <- readFile(name)
+                            _ <- promise.succeed(contents)
+                          yield contents
+                        case RefreshState.AlreadyActive =>
+                          promise.await.debug("Got contents from promise completed by someone else")
 
-
+              yield finalContents
 
 val users = List(
   "Bill",
@@ -68,7 +79,10 @@ val users = List(
 val herdBehavior =
   for
     fileService <- ZIO.service[FileService]
-    _ <- ZIO.foreachPar(users)(user => fileService.retrieveContents("awesomeMemes"))
+    _ <- ZIO.foreachPar(users)(user =>
+      ZIO.debug(s"$user wants to see popular file")
+      fileService.retrieveContents("awesomeMemes")
+    )
   yield ()
 
 object ThunderingHerds extends ZIOAppDefault:
