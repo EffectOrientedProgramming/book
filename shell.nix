e1aca4ca5dd5bc2baf5097e6a65955f2e47aedb4
Bill Frasure <bill.frasure@gmail.com>
2023-01-07 22:28:10 -0700

    1st version of shell.nix :)

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.gitMinimal
+    pkgs.scalafmt
+    pkgs.scala
+    pkgs.sbt
+    pkgs.jetbrains.idea-community
+    # Random others that might be helpful
+    pkgs.vim
+    pkgs.which
+    pkgs.htop
+    pkgs.zlib
+  ];
+}
