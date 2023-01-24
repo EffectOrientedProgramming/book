ab9a3e2a007c674bdbb734ef468a4d4d55d9d2e2
Bill Frasure <bill.frasure@gmail.com>
2023-01-23 23:21:25 -0700

    Work on DataFountain  #bill #time 1h

---
+++ experiments/src/main/scala/streams/Tweets.scala
@@ -0,0 +1,31 @@
+package streams
+
+import zio.{Random, ZIO}
+
+case class Tweets(counter: Counter):
+
+  val randomTweet
+  : ZIO[Any, Nothing, SimpleTweet] =
+    for
+      adjective <- Tweets.randomAdjective
+      id <- counter.get
+    yield SimpleTweet(
+      id,
+      s"ZIO is the $adjective thing ever!"
+    )
+
+private object Tweets:
+  val make: ZIO[Any, Nothing, Tweets] =
+    Counter.make.map(Tweets(_))
+
+  val superlatives =
+    List("best", "greatest", "most awesome")
+  val derogatory =
+    List("worst", "most terrible", "most awful")
+  val allAdjectives = superlatives ++ derogatory
+  val randomAdjective =
+    for index <-
+        Random.nextIntBounded(allAdjectives.size)
+    yield allAdjectives(index)
+
+end Tweets
