0fc956f25ef36f3bb837b71fc4d5947a1f647e30
Bill Frasure <bill.frasure@gmail.com>
2023-04-02 21:36:52 -0600

    +functionality and event stream improvements #bill #time 1h 30m

--- illustratedPrimer/src/main/scala/Hello.scala
+++ illustratedPrimer/src/main/scala/Hello.scala
@@ -1,59 +1,93 @@
 import org.scalajs.dom
 import com.raquo.laminar.api.L.{*, given}
 
-val nameVar: Var[String] = Var(initial = "*Choose a topic*")
+import scala.concurrent.Future
+import concurrent.ExecutionContext.Implicits.global
 
-object KnownTopic:
-  val topics =
-    Set("concurrency", "time", "environment")
-  def apply(topic: String) =
+case class DynamicInfo(content: String)
+val eventBus = new EventBus[DynamicInfo]
+
+case class Backend():
+    def getDynamicInfo(topic: String): Future[DynamicInfo] =
+        Future.successful(DynamicInfo(s"TODO Get ChatGPT info for $topic"))
+
+val backend = Backend()
+
+object Components:
+  def knownTopic(topic: String, topicSelection: Observer[String]) =
     span(
       cls := "known-topic",
-      onClick.mapTo(topic.toLowerCase) --> nameVar,
+      onClick.mapTo(topic.toLowerCase) --> topicSelection,
       topic + " "
     )
 
-  // TODO Handle symbols
-  def recognize(content: String) =
-    val words = content.split(" ")
-    val recognized = words.map { word =>
-      if topics.contains(word.toLowerCase) then KnownTopic(word)
-      else span(word + " ")
-    }
-    recognized
+  def textPiece(
+    text: String,
+               ) =
+  span( text + " ")
 
+  // TODO should not be receiving a full Var here
+  def infoButton(topic: Var[String], infoResults: Observer[Option[DynamicInfo]]) =
+    button(
+      typ := "button",
+      onClick.flatMap(e =>
+        Signal.fromFuture(backend.getDynamicInfo(topic.now()))
+      ) --> infoResults,
+      "More info"
+    )
 
+  def infoDisplay(info: Signal[Option[DynamicInfo]]) =
+
+    div(
+      span("Dynamic info: "),
+      child.text <-- info.map {
+        case Some(value) => value.content
+        case None => "Not available yet"
+      }
+    )
+
+enum ParagraphPiece:
+  case KnownTopic(topic: String)
+  case Text(text: String)
 
 object ActionPanel:
   def apply(topic: Var[String]) =
+    val infoVar: Var[Option[DynamicInfo]] = Var(None)
+
     div(
       cls := "action-panel",
       div(
         child.text <-- topic
       ),
-      div("More info"),
+      Components.infoButton(topic, infoVar.writer),
+      Components.infoDisplay(infoVar.signal),
       div("Examples"),
       div("etc")
     )
 
-val rootElement = div(
+val rootElement = {
+  val nameVar: Var[String] = Var(initial = "*Choose a topic*")
   div(
-    KnownTopic.recognize(
-    """This is a much more flexible approach, where every time a known topic
-      |is mentioned, it is recognized and turned into a link. So if we talk about
-      | accessing environment variables, then environment will be highlighted and
-      | interactive each time.
+    div(
+      KnownTopic.recognize(
+        """This is a much more flexible approach, where every time a known topic
+          |is mentioned, it is recognized and turned into a link. So if we talk about
+          | accessing environment variables, then environment will be highlighted and
+          | interactive each time.
     """.stripMargin
-    )
-  ),
-  ActionPanel(nameVar),
+      ).map {
+        case ParagraphPiece.KnownTopic(topic) => Components.knownTopic(topic, nameVar.writer)
+        case ParagraphPiece.Text(text) => Components.textPiece(text)
+      }
+    ),
+    ActionPanel(nameVar),
 
-)
+  )
+}
 
 // In most other examples, containerNode will be set to this behind the scenes
 val containerNode = dom.document.querySelector("#laminar-app")
 
 object Hello extends App {
-  println("doing stuff")
   render(containerNode, rootElement)
 }
