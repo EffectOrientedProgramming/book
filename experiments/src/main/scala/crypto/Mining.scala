5ec22fc2ec8e8a5fe828d6d49b0c3e965d6a0d11
Bill Frasure <bill.frasure@gmail.com>
2023-05-08 21:25:49 -0600

    fmt

--- experiments/src/main/scala/crypto/Mining.scala
+++ experiments/src/main/scala/crypto/Mining.scala
@@ -11,7 +11,8 @@ import scala.annotation.tailrec
 object Mining extends ZIOAppDefault:
   def run =
     defer {
-      val chain = Ref.make[BlockChain](BlockChain()).run
+      val chain =
+        Ref.make[BlockChain](BlockChain()).run
       raceForNextBlock(chain).repeatN(5).run
       chain.get.debug("Final").run
     }
@@ -29,11 +30,13 @@ object Mining extends ZIOAppDefault:
     defer {
       val raceResult = findNextBlock(miners).run
       val (winner, winningPrime) = raceResult
-      chain.update(chainCurrent =>
-        chainCurrent.copy(blocks =
-          chainCurrent.blocks :+ winningPrime
+      chain
+        .update(chainCurrent =>
+          chainCurrent.copy(blocks =
+            chainCurrent.blocks :+ winningPrime
+          )
         )
-      ).run
+        .run
       debug(
         s"$winner mined block: $winningPrime"
       ).run
@@ -57,12 +60,15 @@ object Mining extends ZIOAppDefault:
       miners: Seq[Miner]
   ): ZIO[Any, Nothing, (String, Int)] =
     defer {
-      val startNum = nextIntBetween(2000, 4000).run
+      val startNum =
+        nextIntBetween(2000, 4000).run
       val result =
-        ZIO.raceAll(
-          miners.head.mine(startNum),
-          miners.tail.map(_.mine(startNum))
-        ).run
+        ZIO
+          .raceAll(
+            miners.head.mine(startNum),
+            miners.tail.map(_.mine(startNum))
+          )
+          .run
       result
     }
 
--- experiments/src/main/scala/direct/EasyMode.scala
+++ experiments/src/main/scala/direct/EasyMode.scala
@@ -7,58 +7,72 @@ import java.io.IOException
 
 // "run this effect" style
 // Note: in ZIOAppDefault "run" conflicts
-val runHello = defer {
-  run(Console.printLine("hello"))
-}
+val runHello =
+  defer {
+    run(Console.printLine("hello"))
+  }
 
 object EasyMode extends ZIOAppDefault:
 
   // note: this needs to be in a defer block
   //       but it is not and results in a runtime error
-  // val r = Console.printLine("hello, world").run
+  // val r = Console.printLine("hello,
+  // world").run
 
-  val d = defer {
-    // Note: It is a compile error to have an unused effect
-    //       in defer which is not run
-    // Console.printLine("hello, world")
+  val d =
+    defer {
+      // Note: It is a compile error to have an
+      // unused effect
+      //       in defer which is not run
+      // Console.printLine("hello, world")
 
-    val c = Console.printLine("hello")
-    val i = Random.nextInt.run
-    Console.printLine(s"i = $i").run
-    c.run
-  }
-
-  val f = for
-    _ <- Console.printLine("hello, world")
-    i <- Random.nextInt
-    _ <- Console.printLine(s"i = $i")
-  yield
-    ()
-
-  val e = defer {
-    Console.printLine("hello").run
-    val r = Random.nextInt.run
-    val a: ZIO[Any, IOException, IO[IOException, Unit]] = defer.info { // .info prints compile-time unraveling
-      Console.printLine("to").run
-      Console.printLine("world").run
-      Console.printLine(s"r = $r") // forgot the .run so this effect is the result of the outer ZIO
+      val c = Console.printLine("hello")
+      val i = Random.nextInt.run
+      Console.printLine(s"i = $i").run
+      c.run
     }
-    Console.printLine("before").run
-    val z = a.run
-    z.run
-  }
+
+  val f =
+    for
+      _ <- Console.printLine("hello, world")
+      i <- Random.nextInt
+      _ <- Console.printLine(s"i = $i")
+    yield ()
+
+  val e =
+    defer {
+      Console.printLine("hello").run
+      val r = Random.nextInt.run
+      val a: ZIO[Any, IOException, IO[
+        IOException,
+        Unit
+      ]] =
+        defer { // .info prints compile-time unraveling
+            Console.printLine("to").run
+            Console.printLine("world").run
+            Console.printLine(
+              s"r = $r"
+            ) // forgot the .run so this effect is the result of the outer ZIO
+          }
+      Console.printLine("before").run
+      val z = a.run
+      z.run
+    }
 
   def get(): ZIO[Any, Nothing, Int] =
     ZIO.succeed(200)
 
   // throw can put something in the error channel
-  val x: ZIO[Any, Exception, String] = defer.info {
-    val response = get().run
-    if response == 200 then
-      "asdf"
-    else
-      throw Exception("zxcv") // or ZIO.fail(Exception("zxcv")).run
-  }
+  val x: ZIO[Any, Exception, String] =
+    defer {
+      val response = get().run
+      if response == 200 then
+        "asdf"
+      else
+        throw Exception(
+          "zxcv"
+        ) // or ZIO.fail(Exception("zxcv")).run
+    }
 
-  override def run =
-    d *> f *> e *> x.debug
+  override def run = d *> f *> e *> x.debug
+end EasyMode
