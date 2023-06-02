a7f0ed7901c073b228e4768f61f56c1e91c77916
James Ward <james@jamesward.com>
2023-06-01 23:36:29 -0600

    cleaner

--- experiments/src/main/scala/zio_intro/OpeningHook.scala
+++ experiments/src/main/scala/zio_intro/OpeningHook.scala
@@ -117,12 +117,10 @@ object Six extends ZIOAppDefault:
 // concurrently save & send analytics
 object Seven extends ZIOAppDefault:
   override def run =
-    val sendAnalytics = userSignedUp("mrsdavis").debug
-
     saveUser("mrsdavis")
       .timeoutFail(TimeoutError)(5.seconds)
       .retry(recurs(3) && spaced(1.second))
       .orElse(sendToManualQueue("mrsdavis"))
       .orElseSucceed("ERROR: User could not be saved")
       .debug
-      .zipPar(sendAnalytics)
+      .zipPar(userSignedUp("mrsdavis").debug)
