5a1abda2191800249ed917e155973e7cc41bfbd9
James Ward <james@jamesward.com>
2023-06-01 23:31:09 -0600

    add concurrency and other variations

--- experiments/src/main/scala/zio_intro/OpeningHook.scala
+++ experiments/src/main/scala/zio_intro/OpeningHook.scala
@@ -17,6 +17,13 @@ val firstIsSlow = Unsafe.unsafe { implicit unsafe =>
   )
 }
 
+val doesNotWorkFirstTime = Unsafe.unsafe { implicit unsafe =>
+  Scenario.WorksOnTry(
+    1,
+    Runtime.default.unsafe.run(Ref.make(0)).getOrThrow()
+  )
+}
+
 enum Scenario:
   case WorksFirstTime
   case DoesNotWork
@@ -24,14 +31,17 @@ enum Scenario:
   case WorksOnTry(attempts: Int, ref: Ref[Int])
 
 def saveUser(username: String, hiddenScenario: Scenario = worksFirstTime): ZIO[Any, DatabaseError.type, String] =
+  val succeed = ZIO.succeed("User saved")
+  val fail = ZIO.fail(DatabaseError).tapError { _ =>
+    Console.printLineError("Database Error").orDie
+  }
+
   defer {
     hiddenScenario match
       case Scenario.WorksFirstTime =>
-        ZIO.succeed("User saved").run
+        succeed.run
       case Scenario.DoesNotWork =>
-        ZIO.fail(DatabaseError).tapError { _ =>
-          Console.printLineError("Database Error").orDie
-        }.run
+        fail.run
 
       case Scenario.FirstIsSlow(ref) =>
         val numCalls = ref.getAndUpdate(_ + 1).run
@@ -39,30 +49,54 @@ def saveUser(username: String, hiddenScenario: Scenario = worksFirstTime): ZIO[A
           ZIO.never.run
         else
           Console.printLineError("Database Timeout").orDie.run
-          ZIO.succeed("User saved").run
+          succeed.run
 
-      case Scenario.WorksOnTry(attempts, ref) => ???
+      case Scenario.WorksOnTry(attempts, ref) =>
+        val numCalls = ref.getAndUpdate(_ + 1).run
+        if numCalls == attempts then
+          succeed.run
+        else
+          fail.run
   }
 
 def sendToManualQueue(username: String): ZIO[Any, TimeoutError.type, String] =
   ZIO.succeed("User sent to manual setup queue")
 
+def userSignedUp(username: String) =
+  ZIO.succeed("Analytics sent")
 
-object First extends ZIOAppDefault:
+
+// works
+object One extends ZIOAppDefault:
   override def run =
     saveUser("mrsdavis")
-      .retry(recurs(3) && spaced(1.second))
+      .debug
+
+// fails
+object Two extends ZIOAppDefault:
+  override def run =
+    saveUser("mrsdavis", doesNotWork)
       .orElseSucceed("ERROR: User could not be saved")
       .debug
 
-object Second extends ZIOAppDefault:
+// fails every time - with retry
+object Three extends ZIOAppDefault:
   override def run =
     saveUser("mrsdavis", doesNotWork)
       .retry(recurs(3) && spaced(1.second))
       .orElseSucceed("ERROR: User could not be saved")
       .debug
 
-object Third extends ZIOAppDefault:
+// fails first time - with retry
+object Four extends ZIOAppDefault:
+  override def run =
+    saveUser("mrsdavis", doesNotWorkFirstTime)
+      .retry(recurs(3) && spaced(1.second))
+      .orElseSucceed("ERROR: User could not be saved")
+      .debug
+
+// first is slow - with timeout and retry
+object Five extends ZIOAppDefault:
   override def run =
     saveUser("mrsdavis", firstIsSlow)
       .timeoutFail(TimeoutError)(5.seconds)
@@ -70,13 +104,25 @@ object Third extends ZIOAppDefault:
       .orElseSucceed("ERROR: User could not be saved")
       .debug
 
-// todo
-object Fourth extends ZIOAppDefault:
+// fails - with retry and fallback
+object Six extends ZIOAppDefault:
   override def run =
-    val username = "mrsdavis"
-    saveUser(username)
+    saveUser("mrsdavis", doesNotWork)
       .timeoutFail(TimeoutError)(5.seconds)
       .retry(recurs(3) && spaced(1.second))
-      .orElse(sendToManualQueue(username))
+      .orElse(sendToManualQueue("mrsdavis"))
       .orElseSucceed("ERROR: User could not be saved")
       .debug
+
+// concurrently save & send analytics
+object Seven extends ZIOAppDefault:
+  override def run =
+    val sendAnalytics = userSignedUp("mrsdavis").debug
+
+    saveUser("mrsdavis")
+      .timeoutFail(TimeoutError)(5.seconds)
+      .retry(recurs(3) && spaced(1.second))
+      .orElse(sendToManualQueue("mrsdavis"))
+      .orElseSucceed("ERROR: User could not be saved")
+      .debug
+      .zipPar(sendAnalytics)
