8c43dda755be06746fc6c7bd16eb45eda903762a
Scala Steward <scala_steward@virtuslab.com>
2023-07-22 13:49:02 +0000

    Update zio-schema, zio-schema-derivation, ... to 0.4.13

--- project/BuildTooling.scala
+++ project/BuildTooling.scala
@@ -298,9 +298,9 @@ object BuildTooling {
     "nl.vroste" %% "rezilience" % "0.9.4",
     "dev.zio" %% "zio-process" % "0.7.2",
     "dev.zio" %% "zio-direct" % "1.0.0-RC7",
-    "dev.zio" %% "zio-schema" % "0.4.12",
-    "dev.zio" %% "zio-schema-json" % "0.4.12",
-    "dev.zio" %% "zio-schema-derivation" % "0.4.12",
+    "dev.zio" %% "zio-schema" % "0.4.13",
+    "dev.zio" %% "zio-schema-json" % "0.4.13",
+    "dev.zio" %% "zio-schema-derivation" % "0.4.13",
     //"org.scala-lang" % "scala-reflect" % scalaVersion.value % "provided"
 //    "dev.zio" %% "zio-direct-streams" % "1.0.0-RC7" // TODO Enable when on the ground :(
   )
