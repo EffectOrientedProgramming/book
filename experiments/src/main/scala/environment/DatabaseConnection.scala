596c55475904ad390833a5d47efe129a046a2115
Bill Frasure <bill.frasure@gmail.com>
2023-08-13 15:36:50 -0600

    Solid first draft of demonstrating the environment using a DatabaseConnection #bill #time 40m

---
+++ experiments/src/main/scala/environment/DatabaseConnection.scala
@@ -0,0 +1,48 @@
+package environment
+
+object DatabaseConnection extends ZIOAppDefault:
+  opaque type UserId = String
+  case class DbConnection(
+      actionLog: Ref[Seq[String]]
+  ):
+    def execute(label: String) =
+      actionLog.update(_.appended(label))
+
+  object DbConnection:
+    val make: ZLayer[Any, Nothing, DbConnection] =
+      ZLayer.scoped(
+          ZIO.acquireRelease(
+            defer {
+              val actionLog: Ref[Seq[String]] = Ref.make(Seq.empty[String]).run
+              val dbConnection = DbConnection(actionLog)
+              dbConnection.execute("OPEN").run
+              dbConnection
+            }
+          )(
+            dbConnection =>
+              defer {
+                dbConnection.execute("CLOSE").run
+                pprint.apply(dbConnection)
+              }.debug
+          )
+      )
+
+  def executeUserQueries(userId: UserId): ZIO[DbConnection, Nothing, Unit] =
+    defer {
+      val dbConnection = ZIO.service[DbConnection].run
+      dbConnection.execute(
+        s"QUERY for $userId preferences".stripMargin).run
+      ZIO.sleep(1.second).run
+      dbConnection.execute(
+        s"QUERY for $userId accountHistory".stripMargin).run
+    }
+
+  def run =
+    defer {
+      executeUserQueries("Alice")
+        .run
+      executeUserQueries("Bob")
+        .run
+    }.provide(
+        DbConnection.make
+      )
