f26a450e074b3529ed66e80d0727ff7a117ffc3f
Bill Frasure <bill.frasure@gmail.com>
2023-11-21 14:26:04 -0700

    +Custom TestAspect that assembles the full Chunk[String] of labels for each case and prints it out.  #bill #time 25m  Try to think of an interesting way to use this information, eg feed it into some stats/AI service to identify patterns

--- experiments/src/test/scala/test_aspects/DemoBaseSpec.scala
+++ experiments/src/test/scala/test_aspects/DemoBaseSpec.scala
@@ -23,17 +23,27 @@ trait DemoBaseSpec extends ZIOSpecDefault {
         ZIO.acquireReleaseWith(before.catchAllCause(c => ZIO.fail(TestFailure.Runtime(c))))(after)(_ => test)
     }
 
+  def walk[R, E](spec: Spec[R, E], labels: Chunk[String] = Chunk.empty): Unit =
+    println("Walking")
+    spec.caseValue match
+      case Spec.ExecCase(exec, spec) => ()
+      case Spec.LabeledCase(label, spec) =>
+        println("Walk Label: " + label)
+        walk(spec, labels.appended(label))
+      case Spec.ScopedCase(scoped) => ()
+      case Spec.MultipleCase(specs) =>
+        println("Multi case")
+        specs.foreach(s => walk(s, labels))
+      case Spec.TestCase(test, annotations) =>
+        println("test labels: " + labels.mkString(" - "))
+        ()
+
   def aroundAllWith[R0, E0, A0](
                                  before: ZIO[R0, E0, A0]
                                )(after: A0 => ZIO[R0, Nothing, Any]): TestAspect[Nothing, R0, E0, Any] =
     new TestAspect[Nothing, R0, E0, Any] {
       def some[R <: R0, E >: E0](spec: Spec[R, E])(implicit trace: Trace): Spec[R, E] =
-        spec.caseValue match
-          case Spec.ExecCase(exec, spec) => ()
-          case Spec.LabeledCase(label, spec) => println("Label: " + label)
-          case Spec.ScopedCase(scoped) => ()
-          case Spec.MultipleCase(specs) => ()
-          case Spec.TestCase(test, annotations) => ()
+        walk(spec)
         Spec.scoped[R](
           ZIO.acquireRelease(before)(after).mapError(TestFailure.fail).as(spec)
         )
