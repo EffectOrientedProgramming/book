6a625309741eeae3271748cda8e662a62bfe7924
Bill Frasure <bill.frasure@gmail.com>
2024-01-01 22:49:08 -0700

    Significantly trim down Hedging #bill #time 1h The stats tracking I was doing was *very* distracting to the main point.

--- experiments/src/main/scala/performance/Hedging.scala
+++ experiments/src/main/scala/performance/Hedging.scala
@@ -4,131 +4,60 @@ import zio_helpers.repeatNPar
 
 object Hedging extends ZIOAppDefault:
 
-  extension [R, E, A](z: ZIO[R, E, A])
-    def hedge(
-        wait: zio.Duration,
-        depth: Int = 1
-    ): ZIO[R, E, A] =
-      depth match
-        case 0 =>
-          z
-        case other =>
-          z.delay:
-              wait
-            .race:
-              hedge(wait, depth - 1)
-
-  def hedgedRequestGeneral =
-    handleRequest.hedge(25.millis, 3)
-
   def run =
     defer:
-      val timeBuckets =
-        Ref
-          .make[Map[Percentile, RequestStats]]:
-            Map()
-          .run
+      val fastResponses =
+        Ref.make(0).run
+      val slowResponses =
+        Ref.make(0).run
 
       ZIO
         .repeatNPar(50_000):
           _ =>
-            demoRequest:
-              timeBuckets
+            defer:
+              Response
+                .random
+                // .hedge(25.millis)
+                .run match
+                case Response.Fast =>
+                  fastResponses.update(_ + 1).run
+                case Response.BreachOfContract =>
+                  slowResponses.update(_ + 1).run
         .run
 
-      pprint.pprintln(
-        timeBuckets.get.run,
-        width = 47
-      )
-
-  def demoRequest(
-      timeBuckets: Ref[
-        Map[Percentile, RequestStats]
-      ]
-  ) =
-    hedgedRequestGeneral.tap(requestTime =>
-      timeBuckets.update(results =>
-        results.updatedWith(
-          Percentile
-            .fromDuration(requestTime.duration)
-        ):
-          case Some(value) =>
-            Some(
-              value.copy(
-                count = value.count + 1,
-                totalTime =
-                  value
-                    .totalTime
-                    .plus(requestTime.duration)
-              )
-            )
-          case None =>
-            Some(
-              RequestStats(
-                count = 1,
-                totalTime = requestTime.duration
-              )
-            )
-      )
-    )
+      fastResponses.get
+        .debug("Fast responses")
+        .run
+      slowResponses
+        .get
+        .debug("Slow responses")
+        .run
 
 end Hedging
 
-val handleRequest =
-  defer {
-    Percentile.random.run match
-      case Percentile._50 =>
-        ResponseTimeCutoffs.Fast
-      case Percentile._95 =>
-        ResponseTimeCutoffs.Acceptable
-      case Percentile._999 =>
-        ResponseTimeCutoffs.BreachOfContract
-  }.tap(dimension =>
-    ZIO.sleep(dimension.duration)
-  )
+extension [R, E, A](z: ZIO[R, E, A])
+  def hedge(
+             wait: zio.Duration,
+           ): ZIO[R, E, A] =
+    z.race:
+      z
+        .delay:
+          wait
 
-// TODO Fix name
-enum ResponseTimeCutoffs(val duration: Duration):
-  case Fast
-      extends ResponseTimeCutoffs(21.millis)
-  case Acceptable
-      extends ResponseTimeCutoffs(70.millis)
-  case BreachOfContract
-      extends ResponseTimeCutoffs(1800.millis)
+enum Response:
+  case Fast, BreachOfContract
 
-enum Percentile:
-  case _50,
-    _95,
-    _999
-
-object Percentile:
-  def random =
+object Response:
+  def random: ZIO[Any, Nothing, Response] =
     defer:
-      val int = Random.nextIntBounded(1_000).run
-      if (int < 950)
-        Percentile._50
-      else if (int < 999)
-        Percentile._95
-      else
-        Percentile._999
-
-  def fromDuration(d: Duration) =
-    d match
-      case ResponseTimeCutoffs.Fast.duration =>
-        Percentile._50
-      case ResponseTimeCutoffs
-            .Acceptable
-            .duration =>
-        Percentile._95
-      case ResponseTimeCutoffs
-            .BreachOfContract
-            .duration =>
-        Percentile._999
-      case _ =>
-        ???
-end Percentile
-
-case class RequestStats(
-    count: Int,
-    totalTime: Duration
-)
+      val random = Random.nextIntBounded(1_000).run
+      random match
+        case 0 =>
+          ZIO.sleep:
+            3.seconds
+          .run
+          ZIO.succeed:
+            Response.BreachOfContract
+          .run
+        case _ =>
+          Response.Fast
