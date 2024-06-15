78d6a21b800c8036f6910286d77280d74250635d
Bill Frasure <bill.frasure@gmail.com>
2024-06-14 21:11:37 -0600

    More brittle compile error rewriting #bill #time 15m

--- src/main/scala/mdoc/Main.scala
+++ src/main/scala/mdoc/Main.scala
@@ -744,8 +744,28 @@ def cleanupZioErrorOutput(raw: String) =
     else
       filtered
 
+  val effectCantFailMsg =
+  """
+    |This error handling operation assumes your effect can fail. However, your effect has Nothing for the error type, which means it cannot fail, so there is no need to handle the failure. To find out which method you can use instead of this operation, please see the reference chart at: https://zio.dev/can_fail.
+    |I found:
+    |
+    |    CanFail.canFail[E](/* missing */summon[scala.util.NotGiven[E =:= Nothing]])
+    |
+    |But no implicit values were found that match type scala.util.NotGiven[E =:= Nothing].
+    |""".stripMargin
+
+  val effectCantFailMsgSimple =
+    """
+      |This error handling operation assumes your effect
+      |can fail. However, your effect has Nothing for the
+      |error type, which means it cannot fail, so there
+      |is no need to handle the failure.
+      |""".stripMargin
+
+
   withoutTrailingBlankLine
     .mkString("\n")
+    .replace(effectCantFailMsg, effectCantFailMsgSimple)
     .replace(
       "error:\n\n\n──── ZLAYER ERROR ────────────────────────────────────────────────────",
       "──── ZLAYER ERROR ───────────"
